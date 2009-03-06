697a1ecc985cef9ee6937ce22f238d3f22329661
Bart van Leeuwen <aidil@wotf.org>
2009-03-06 20:10:46 +0000

    tell command supports mudnames with spaces in them by putting quotes around the user@mud argument

--- lib/cmds/player/tell.c
+++ lib/cmds/player/tell.c
@@ -1,19 +1,20 @@
-void main( string who ) {
+ void main( string who ) {
   object *usr;
   int i;
   int flag;
   string what;
   string where;
 
-  sscanf( who, "%s %s", who, what );
+  if(sscanf(who, "\"%s\" %s", who, what) != 2) {
+    sscanf( who, "%s %s", who, what );
+  }
 
-  if( sscanf( who, "%s@%s", who, where ) == 2 ) {
+  if( sscanf( who, "%s@%s", who, where) == 2 ) {
     /* intermud tell */
     IMUD_D->do_tell( who, where, what );
     write( "You tell " + capitalize(lowercase(who)) + "@" + where + ": " + what + "\n" );
   } else {
     usr = users();
-    flag = 0;
     for( i = 0; i < sizeof( usr ); i++ ) {
       if( usr[i]->query_name() == lowercase(who) ) {
 	usr[i]->query_player()->message( capitalize(this_player()->query_name()) + " tells you: " + what + "\n" );
