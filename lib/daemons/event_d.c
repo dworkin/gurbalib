6dd7a82554882449705ad7f8beeb1c820cffc9fd
Cerihan <cerihan@3f2e853b-fc08-de11-a6a9-00e0812c4779>
2009-03-13 06:45:22 +0000

    Port over event daemon fixes from the other devel tree.  Makes the event_d more fault tolerant.

--- lib/daemons/event_d.c
+++ lib/daemons/event_d.c
@@ -23,20 +23,32 @@ void unsubscribe_event( string name ) {
   global_events[name] -= ({ previous_object() });
 }
 
+void dispatch_event( string name, object *obs, int i, int max_i, mixed *args ) {
+  /* First see if there are any more objects after this one */
+  if ( i < max_i ) {
+    /* Go ahead and dispatch for the next one to make sure all get the event
+       in case of errors */
+    call_out("dispatch_event", 0, name, obs, i + 1, max_i, args );
+  }
+  if( obs[i] ) {
+    if ( sizeof(args) > 0 )
+      call_other( obs[i], "event_" + name, args );
+    else
+      call_other( obs[i], "event_" + name );
+  }
+}
+
+
 void event( string name, varargs mixed args... ) {
   object *obs;
   int i;
 
+  /* Make sure that all nil objects are gone */
   global_events[name] -= ({ nil });
   obs = global_events[name];
 
-  if( !obs )
+  if( !obs || sizeof(obs) == 0 )
     return;
 
-  for( i = 0; i < sizeof( obs ); i++ ) {
-    if(sizeof(args) > 0)
-      call_other( obs[i], "event_" + name, args );
-    else
-      call_other( obs[i], "event_" + name);
-  }
+  dispatch_event( name, obs, 0, sizeof(obs) - 1, args );  
 }
