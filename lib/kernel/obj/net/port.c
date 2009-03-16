7a07936f1b6e81118db72c44171a0b0fdf0ffd9a
Bart van Leeuwen <aidil@wotf.org>
2009-03-16 23:00:50 +0000

    It does help to also add the new files..

---
+++ lib/kernel/obj/net/port.c
@@ -0,0 +1,138 @@
+/*
+ * GurbaLib port object
+ * Inspired by the DGD kernel lib and the Way of the Force kernel
+ *
+ * March 2009, Aidil@Way of the Force
+ */
+
+#include <status.h>
+#include <ports.h>
+
+private object user;            /* our user object */
+private int port;               /* our port number */
+private int blocked;            /* port blocked? */
+private string protocol;        /* telnet or tcp */
+
+
+void set_user(object u) {
+  if(previous_program() == M_PORT) {
+    user = u;
+  }
+}
+
+object query_user() {
+  return user;
+}
+
+static void create() {
+  protocol = nil;
+  user = nil;
+  blocked = 0;
+  port = 0;
+}
+
+private void set_protocol(string proto) {
+  if(protocol) {
+    return;
+  } else {
+    switch(proto) {
+      case "telnet" :
+      case "tcp" :
+      case "udp" :
+        protocol = proto;
+        break;
+      default :
+        error("Unknown protocol");
+        break;
+    }
+  }
+}
+
+static void _open(mixed * tls, int p) {
+  port = p;
+  if(user) user->_F_open(p);
+}
+
+void open(int p) {
+  _open(DRIVER->new_tls(), p);
+}
+
+static void _close(mixed * tls, varargs int force) {
+  if(user) {
+    catch(user->_F_close(force));
+  }
+  if(!force) {
+    destruct_object(this_object());
+  }
+}
+
+void close(varargs int force) {
+  _close(DRIVER->new_tls(), force);
+}
+
+#ifdef SYS_NETWORKING
+void open_port(string proto, varargs mixed p) {
+  if(previous_object() == user) {
+    console_msg("Opening port "+proto+":"+p+"\n");
+    set_protocol( proto );
+    if( !intp( p ) ) {
+      ::open_port( proto );
+    } else {
+      if( p < 1 || p > 65535 ) {
+        error("Bad port "+p+" in open_port()");
+      }
+      ::open_port( proto, p );
+    }
+  }
+}
+
+static object _connection(mixed * tls, string ip, int port) {
+  object ob;
+
+  if(user) {
+    ob = user->_F_connection(ip, port);
+    return ob;
+  } else {
+    error("No user set in port object!");
+  }
+}
+
+object connection(string ip, int port) {
+  return _connection(DRIVER->new_tls(), ip, port);
+}
+
+int datagram(string str) {
+  int len;
+
+  if(previous_object() != user) {
+    error("Illegal call to port object");
+  }
+  if( protocol != "udp" || !port ) {
+    error("Bad protocol or port not open");
+  }
+}
+
+static void _datagram_done(mixed * tls) {
+}
+
+static void datagram_done() {
+  _datagram_done(DRIVER->new_tls());
+}
+
+static void _receive_error(mixed * tls, string err) {
+  console_msg(err+"\n");
+  if(user) user->_F_receive_error(err);
+}
+
+static void receive_error(string err) {
+  _receive_error(DRIVER->new_tls(), err);
+}
+
+static void _receive_datagram(mixed * tls, string str, string ip, int p) {
+  if(user) user->_F_receive_datagram(ip,p);
+}
+
+static void receive_datagram(string str, string ip, int p) {
+  _receive_datagram(DRIVER->new_tls(), str, ip, p);
+}
+#endif
---
+++ lib/std/modules/m_port.c
@@ -0,0 +1,75 @@
+/*
+ * Gurbalib port module
+ *
+ * This must be inherited by anything that wants to listen
+ * on a port or use udp.
+ *
+ * udp is currently not implemented by the port object.
+ *
+ * Aidil @ Way of the Force
+ *
+ */
+
+private object "port" port_object;
+private int port_number;
+
+/*
+ * For use by the object inheriting us
+ *
+ */
+nomask static void open_port(string proto, varargs mixed port) {
+  if(port_object) {
+    console_msg("Port already open: "+proto+":"+(intp(port) ? (string) port : "*")+"\n");
+  } else {
+    port_object = clone_object(PORT);
+    port_object->set_user(this_object());
+    port_object->open_port(proto, port);
+  }
+}
+
+nomask static void close_port() {
+  if(port_object) {
+    destruct_object(port_object);
+  }
+}
+
+nomask static int send_datagram(string str) {
+  if(port_object) {
+    return port_object->datagram(str);
+  }
+}
+
+/* 
+ * For use by the port object only
+ *
+ */
+nomask object _F_connection(string ip, int port) {
+  if((object "port") previous_object() == port_object) {
+    return call_other(this_object(), "connection", ip, port);
+  }
+}
+
+nomask void _F_open(varargs int port) {
+  if((object "port") previous_object() == port_object) {
+    port_number = port;
+    call_other(this_object(), "open", port);
+  }
+}
+
+nomask void _F_close(varargs int force) {
+  if((object "port") previous_object() == port_object) {
+    call_other(this_object(), "close", force);
+  }
+}
+
+/*
+ * Public functions
+ */
+ 
+nomask object "port" query_port_object() {
+  return port_object;
+}
+
+nomask int query_port_number() {
+  return port_number;
+}
