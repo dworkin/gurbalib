93878ac43812fe6067c3bd7f96a6262f955a1a36
Bart van Leeuwen <aidil@wotf.org>
2009-03-17 11:16:49 +0000

    Updated the man command to display a topic index when no argument provided or the argument is a dir instead of a file

--- lib/cmds/wiz/man.c
+++ lib/cmds/wiz/man.c
@@ -7,11 +7,12 @@ void main( string arg ) {
   string file;
   string *tmp;
   string *lines;
+  string header;
   int i;
   int where;
 
-  if ( !arg || arg == "" ) {
-    arg = "intro";
+  if ( !arg ) {
+    arg = "";
   }
 
   if( SECURE_D->query_wiz(this_player()->query_name()) != 1) {
@@ -22,11 +23,11 @@ void main( string arg ) {
   
   file = normalize_path( arg, "/doc/" );
   
-  if( file_exists( file ) < 1 ) {
+  if( file_exists( file ) == 0 ) {
     file = normalize_path( arg, "/doc/afun/");
-    if( file_exists(file) < 1 ) {
+    if( file_exists(file) == 0 ) {
       file = normalize_path( arg, "/doc/kfun/");
-      if(file_exists(file) < 1 ) {
+      if(file_exists(file) == 0 ) {
         write( capitalize(arg) + ": Unknown man page." );
         write_file("/logs/man", capitalize(this_player()->query_name()) + " on " + ctime(time()) + ": " + arg + "\n");
         return;
@@ -34,10 +35,30 @@ void main( string arg ) {
     }
   }
   
-  tmp = explode( read_file( file ), "\n" );
+  if(file_exists(file) > 0) {
+    header = "Manpage for "+arg+".";
+    tmp = explode( read_file( file ), "\n" );
+  } else {
+    string * stuff;
+    int j;
+
+    header = "Index for "+arg+".";
+    tmp = ({ });
+    stuff = get_dir(file+"/*")[0];
+    stuff -= ({ ".svn" });
+    for(j=0;j<sizeof(stuff);j += 4) {
+      string l;
+      l = "";
+      for(i=0;i<4;i++) {
+        if(j+i < sizeof(stuff)) l += (stuff[j+i]+"                          ")[0..18];
+      }
+      tmp += ({ l });
+    }
+  }
+
   lines = ({ });
-  write("Manpage for " + capitalize(arg) + ".\n");
-  for( i = 0; i < strlen("Manpage for " + arg + "."); i++){
+  write(header + "\n");
+  for( i = 0; i < strlen(header); i++){
     out("-");
   }
   write("\n");
