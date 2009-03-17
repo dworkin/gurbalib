2333b116d1e7a569e564eba77308fa2946b631d4
Bart van Leeuwen <aidil@wotf.org>
2009-03-17 23:36:01 +0000

    Changed eval command syntax, if you want to display the return of some expression, you explicitly have to include a return statement, ie: 'eval return this_player()', otherwise nothing is returned. This fixes the 'void value not ignored' errors

--- lib/cmds/wiz/eval.c
+++ lib/cmds/wiz/eval.c
@@ -18,7 +18,7 @@ void main( string str ) {
 	      "mixed exec(object player, varargs mixed argv...) {\n" +
 	      "    mixed " +
 	      "a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z;\n\n" +
-	      "    return(" + str + ");\n}\n");
+	      str + ";\n}\n");
   err = catch(obj = compile_object(filename),
 	      result = obj->exec(this_player(), str)
 	      );
