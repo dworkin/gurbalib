8c9f68224b6b4586d77e627a15b8b9930dda2190
Bart van Leeuwen <aidil@wotf.org>
2009-03-21 03:12:51 +0000

    block banning of 127.*.*.* and variations, commented out some other networks that couldn't be banned

--- lib/cmds/wiz/ban.c
+++ lib/cmds/wiz/ban.c
@@ -3,10 +3,19 @@ void main( string str ) {
       write("Ban which site? ( example: 1.2.*.* )\n");
       return;
    }
+/*
    if( str == "35.8.*.*" || str == "152.*.*.*" ) {
       write("Ban "+str+"?!? You must be kidding...\n");
       return;
    }
+*/
+#ifndef ALLOW_SHOOTING_ONESELF
+  if( str == "127.*.*.*" || str == "127.0.*.*" || str == "127.0.0.*" || str == "127.0.0.1" ) {
+      write("Ban "+str+"?!? You must be kidding...\n");
+      return;
+   }
+#endif
+
    if( SITEBAN_D->siteban(str) ) {
       this_player()->simple_action( "$N $vban the site: "+str+"\n" );
    } else {
