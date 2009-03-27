01fa5cfd983f298506e39153afc9ddcfeaf4a641
Bart van Leeuwen <aidil@wotf.org>
2009-03-27 10:09:56 +0000

    Change the home command to default to /wiz/<name>/rooms/workroom. If it doesn't exist, fallback to /wiz/<name>/workroom and complain about it

--- lib/cmds/wiz/home.c
+++ lib/cmds/wiz/home.c
@@ -1,10 +1,25 @@
 void main( string str ) {
 
-  string error;
+  string error, name;
   object env;
   string filename;
 
-  filename = "/wiz/" + this_player()->query_name() + "/workroom";
+  name = this_player()->query_name();
+
+  filename = "/wiz/" + name + "/rooms/workroom";
+
+  if( !file_exists( filename + ".c" ) ) {
+    filename = "/wiz/" + name + "/workroom";
+
+    if( file_exists( filename + ".c" ) ) {
+      if( file_exists( "/wiz/" + name + "/rooms" ) == 0 ) {
+        write("Please create a /wiz/"+name+"/rooms directory and "+
+              "move your workroom into it.");
+      } else {
+        write("Please move your workroom into /wiz/"+name+"/rooms/");
+      }
+    }
+  }
 
   if( !file_exists( filename + ".c" ) ) {
     write( "Make one first.\n" );
