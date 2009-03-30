2c007edae4cf815abc8dd59b6cb63dc7f895af5b
Bart van Leeuwen <aidil@wotf.org>
2009-03-30 13:58:26 +0000

    Improved the zap command (Frutsel), made it more efficient (Aidil)

--- lib/cmds/wiz/zap.c
+++ lib/cmds/wiz/zap.c
@@ -1,26 +1,77 @@
 /* kill something ensuring a corpse is left behind
      -- Arron Cusimano (mordain) 20090326 */
+
 void main( string str ) {
   object target;
-  object *obs;
   int i, sz;
+  int target_hp, target_damage;
+  string who, target_name;
+  int perc;
 
   if ( !str || str == "" ) {
-    write("zap someone?");
+    write("Zap someone?");
     return;
   }
 
-  obs = this_environment()->query_inventory();
-  for ( i = 0, sz = sizeof(obs); i < sz && !target; ++i ) {
-    if (obs[i]->query_name() == str)
-      target = obs[i];
+  if( sscanf( str, "%s %d", who, perc ) != 2 ) {
+    who = str;
+    perc = 100;
   }
+
+
+  if (perc <= 0) {
+      write("Zapping with no power is useless.");
+      return;
+  }
+  if (perc > 100) {
+      write("Zapping with more than 100% damage is silly; setting to 100%.");
+      perc = 100;
+  }
+
+  target = this_environment()->present(who);
 
   if (!target) {
-    write("Cant find " + str + "\n");
+    write("Cant find " + who + "\n");
     return;
   }
 
-  /* receive_damage()  from combat.c, this looks ugly on this_player()'s side, but looks good on the targets side */
-  target->receive_damage( this_player(), target->query_max_hp(), 0 );
+  target_hp = target->query_hp();
+  target_name = target->query_id();
+  target_damage = (target_hp * perc) / 100;
+
+  write(target->query_id() + " has " + target_hp + " hitpoints");
+  write(target->query_id() + " is going to receive " + target_damage + " damage");
+
+    switch(perc) {
+      case 1..20 : this_player()->targetted_action("$N $vlook slightly annoyed at $T.", target);
+                   target->message( "You feel uncomfortable.");
+                   break;
+      case 21..40 : this_player()->targetted_action( "$N $vlook intensely at $T.", target);
+                    target->message( "Your skin burns.");
+                    break;
+      case 41..60 : this_player()->targetted_action( "$N $vlook angry at $T", target);
+                    target->message( "You feel as if you're hit by a tree.");
+                    break;
+      case 61..80 : this_player()->targetted_action( "$N $vstare at $T for a while", target);
+                    target->message( "Your head hurts, and your nose begins to bleed.");
+                    break;
+      case 81..99 : this_player()->targetted_action( "Sparks fly as $N $vstare intensely at $T", target);
+                    target->message( "Your brain starts to ooze out of your ears.");
+                    break;
+      case 100 : this_player()->targetted_action( "$N $vgive $T the evil eye.", target);
+                 target->message("Your head explodes.");
+                 break;
+    }
+  /*this_player()->simple_action("$N $vgive " + target->query_id() + " a very nasty look..."); */
+  target->decrease_hp(target_damage);
+
+  if (target->query_hp() < 1) {
+    target->simple_action( target->query_id() + " $vfall to the ground...dead." );
+    target->message( "You have died." );
+    target->die();
+  } else {
+    write(target->query_id() + " is left with " + target->query_hp() + " hitpoints.");
+  }
+
 }
+
