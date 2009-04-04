e978230e25b23b263990f87ffb140a77aaa3c299
Bart van Leeuwen <aidil@wotf.org>
2009-04-04 15:19:35 +0000

    Change the 'goto' command to properly setup a room after loading it. Change the out exit of the void to use the STARTING_ROOM define

--- lib/cmds/wiz/goto.c
+++ lib/cmds/wiz/goto.c
@@ -1,5 +1,6 @@
 /*Made by Aphex on Fri 21 Nov 97*/
 /* Fixed to handle goto'ing of room files by Fudge. */
+/* Fixed to properly setup rooms that get loaded by this command */
 
 void main( string str ) {
   object usr;
@@ -12,17 +13,30 @@ void main( string str ) {
       player = usr->query_player();
       this_player()->move(player->query_environment()->base_name());
       this_player()->query_environment()->tell_room( this_player(), capitalize(this_player()->query_name()) + " appears from nowhere.\n");
-      this_player()->do_look();
+      this_player()->do_look(0);
     } else { 
       out( "Going somewhere?\n");
     }
   } else {
 
     if( file_exists( normalize_path( str, this_player()->query_env( "cwd" ) ) + ".c" ) > 0 ) {
+      object ob;
+      string file;
+
+      file = normalize_path( str, this_player()->query_env( "cwd" ) ) + ".c";
+      if(!(ob = find_object( file ) ) ) {
+        catch {
+          ob = compile_object( file );
+          ob->setup();
+          ob->setup_mudlib();
+        } : {
+          write("Could not load "+str);
+        }
+      }
 
       this_environment()->event( "body_leave", this_player() );
       this_environment()->tell_room( this_player(), capitalize(this_player()->query_name()) + " disappears.\n" );
-      if( !this_player()->move( normalize_path( str, this_player()->query_env( "cwd" ) ) ) ) {
+      if( !ob || !this_player()->move( ob ) ) {
 	write( "\nConstruction blocks your path.\n" );
       }
       this_player()->do_look( 0 );
--- lib/rooms/void.c
+++ lib/rooms/void.c
@@ -5,6 +5,6 @@ void setup( void ) {
   set_brief( "%^MAGENTA%^The void%^RESET%^" );
   set_long( "If you're in here unintentionally, something bugged out. Report what you did to the staff." );
   set_exits( ([ 
-	       "out" : "/domains/gurba/town_square",
+	       "out" : STARTING_ROOM,
   ]) );
 }
