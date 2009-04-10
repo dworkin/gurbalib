f087a91faa00b57bb91cbfb55367c5d696755833
Bart van Leeuwen <aidil@wotf.org>
2009-04-10 22:29:56 +0000

    Added rexplode(), a reversible explode string safun

---
+++ lib/sys/safun/rexplode.c
@@ -0,0 +1,24 @@
+/*
+ * reversible explode string
+ *
+ */
+
+static string * rexplode( string str, string sep ) {
+  string dummy;
+  string * result;
+
+  argcheck( stringp( str ), 1, "non empty string" );
+  argcheck( stringp( sep ), 2, "non empty string" );
+
+  if( sep == "!" ) {
+    dummy = "?";
+  } else {
+    dummy = "!";
+  }
+
+  str = dummy + str + dummy;
+  result = explode( str, sep );
+  result[0] = result[0][1..];
+  result[sizeof(result)-1] = result[sizeof(result)-1][..strlen(result[sizeof(result)-1])-2];
+  return result;
+}
