c4fd6766cf86c5618cac080b6cb25ebd20c43403
Nullinfinite <null.infinite@gmail.com>
2009-05-25 02:05:07 +0000

    eval now uses compile_object with optional code arg, instead of using write_file.  It has been enhanced so that a user may or may not end his src argument with ';' , code will compile fine either way now. -nullinfinite

--- lib/cmds/wiz/eval.c
+++ lib/cmds/wiz/eval.c
@@ -1,27 +1,25 @@
-void main( string str ) {
-  string filename;
+void main( string src, varargs mixed args...) {
   object obj;
   string err;
   mixed result;
+  int str_size;
 
-  filename = "/tmp/" + this_player()->query_name();
-
-  remove_file( filename + ".c" );
-
-  str = replace_string( str, ".me", "this_player()" );
-  str = replace_string( str, ".here", "this_environment()" );
-
-  write_file( filename + ".c" ,
+  src = replace_string( src, ".me", "this_player()" );
+  src = replace_string( src, ".here", "this_environment()" );
+  str_size = strlen(src);
+  if(str_size && src[ (str_size-1)] != ';')
+    src += ";";
+  err = catch(obj = compile_object("eval_ob", 
 	      "# include <float.h>\n# include <limits.h>\n" +
 	      "# include <status.h>\n# include <trace.h>\n" +
 	      "# include <type.h>\n\n" +
 	      "mixed exec(object player, varargs mixed argv...) {\n" +
+		  "int ticks;\n"+
 	      "    mixed " +
-	      "a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z;\n\n" +
-	      str + ";\n}\n");
-  err = catch(obj = compile_object(filename),
-	      result = obj->exec(this_player(), str)
-	      );
+	      "a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z;\n"+
+		  "argv = argv[0];\n" +
+	      src + "\n}"),
+		result = obj->exec(this_player(), args) );
 
   if( obj )
     destruct_object( obj );
@@ -30,11 +28,10 @@ void main( string str ) {
     write("Error: " + err + ".\n");
   } else {
     if(result || intp(result)) {
-      write( "Result: \n" + dump_value(result, ([ ])) + "\n" );
+      write( "Result: \n" + dump_value(result) + "\n" );
     } else {
       write( "Result: \n" + "<nil>" + "\n" );
     }
-  }    
-  remove_file( filename + ".c" );
+  } 
 
 }
