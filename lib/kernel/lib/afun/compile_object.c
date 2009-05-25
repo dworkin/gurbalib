72ffc638e7f2b3d2607f8d7fce235507eb5bb1aa
Nullinfinite <null.infinite@gmail.com>
2009-05-25 01:58:38 +0000

    changed so that compile_object afun wont call allow_object when code is compiled from source instead of file.

--- lib/kernel/lib/afun/compile_object.c
+++ lib/kernel/lib/afun/compile_object.c
@@ -5,7 +5,7 @@ static object compile_object( string path, varargs string code ) {
     if( path[strlen(path)-2] == '.' && path[strlen(path)-1] == 'c' )
       path = path[..strlen(path)-3];
   }
-  if(find_object(COMPILER_D)) {
+  if(find_object(COMPILER_D) && !code) {
     path = COMPILER_D->allow_object( path );
   }
   return driver->compile_object( path, code );
