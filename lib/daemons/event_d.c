e5f5f376aba801844763570687420573d8406fcb
Bart van Leeuwen <aidil@wotf.org>
2009-06-01 17:32:21 +0000

    Better protection against running out of ticks during event handling, added some optional debugging output to the event_d (add #define DEBUG_EVENT_D to /include/local_config.h to enable)

--- lib/daemons/event_d.c
+++ lib/daemons/event_d.c
@@ -1,11 +1,16 @@
 #include <status.h>
 
+#ifdef DEBUG_EVENT_D
+#define DB(X) console_msg("eventd: "+X+"\n")
+#else
+#define DB(X) 
+#endif
 
-#define RT status()[ST_TICKS]
-#define MT (MAX_TICKS/2)
+#define REMAINING_TICKS status()[ST_TICKS]
 
 static mapping global_events;
 int eventid;
+int dummy;
 
 void create( void ) {
   global_events = ([ ]);
@@ -33,44 +38,46 @@ void unsubscribe_event( string name ) {
   global_events[name][previous_object()] = nil;
 }
 
-void dispatch_event( string name, object *obs, int * counters, mixed *args ) {
-  int i, max_i, guard_handle;
+void dispatch_event( string name, object *obs, int * counters, int id, mixed *args ) {
+  int i, max_i, ticks_used, guard_handle;
 
-  eventid++;
+  dummy++;
 
   i = counters[0];
   max_i = counters[1];
 
-#ifdef DEBUG_EVENT_D
-  if(i == max_i)
-    console_msg("dispatch_event( \""+name+"\" ) : "+(i+1)+" out of "+(max_i+1)+"\n");
-#endif
+  DB("dispatch: "+name+" ("+id+") : starting at "+(i+1)+" out of "+(max_i+1));
 
   /* First see if there are any more objects after this one */
   if ( i < max_i ) {
     /* Go ahead and dispatch for the next one to make sure all get the event
        in case of errors */
-    guard_handle = call_out("dispatch_event", 0, name, obs, counters, args );
+    guard_handle = call_out("dispatch_event", 0, name, obs, counters, id, args );
   } else {
     guard_handle = 0;
   }
 
-  while( RT > MT && i <= max_i ) {
-    counters[0] = i + 1;
+  rlimits( MAX_DEPTH; -1 ) {
+    while( (ticks_used < (2*MAX_TICKS)/3) && (i <= max_i) ) {
+      counters[0] = i + 1;
 
-    if( obs[i] ) {
-      if ( sizeof(args) > 0 )
-        call_other( obs[i], "event_" + name, args );
-      else
-        call_other( obs[i], "event_" + name );
+      rlimits( MAX_DEPTH; MAX_TICKS) {
+        if( obs[i] ) {
+          if ( sizeof(args) > 0 )
+            call_other( obs[i], "event_" + name, args );
+          else
+            call_other( obs[i], "event_" + name );
+        }
+        ticks_used += (MAX_TICKS - REMAINING_TICKS);
+      }
+      i++;
     }
-    i++;
   }
-#ifdef DEBUG_EVENT_D
-  console_msg("dispatch_event: "+name+": next: "+(counters[0]+1)+" out of "+(counters[1]+1)+"\n");
-#endif
   if(guard_handle && (counters[0] > counters[1])) {
+    DB("dispatch: "+name+" ("+id+") : finished with "+(i)+" out of "+(max_i+1)+" subscribers, removing guard call_out.");
     remove_call_out(guard_handle);
+  } else {
+    DB("dispatch: "+name+" ("+id+") : suspending, next subscriber: "+(counters[0]+1)+" out of "+(counters[1]+1));
   }
 }
 
@@ -81,13 +88,18 @@ void event( string name, varargs mixed args... ) {
 
   if(!global_events[name]) return;
 
+  eventid++;
+
   obs = map_indices(global_events[name]);
 
   if( !obs || sizeof(obs) == 0 )
     return;
 
-#ifdef DEBUG_EVENT_D
-  console_msg("Starting dispatch chain for event_"+name+" ("+eventid+"), "+sizeof(obs) +" subscribers.\n"); 
-#endif
-  dispatch_event( name, obs, ({ 0, sizeof(obs) - 1 }), args );  
+  DB("event: "+name+" ("+eventid+"), "+sizeof(obs) +" subscribers."); 
+  dispatch_event( name, obs, ({ 0, sizeof(obs) - 1 }), eventid, args );  
 }
+
+void upgraded() {
+  eventid = 0;
+}
+
