9a95a6061641d6276de8a6730c45ee13d7d366b3
Nullinfinite <null.infinite@gmail.com>
2009-06-12 14:00:08 +0000

    handle words starting with honor/honour.

--- lib/kernel/lib/afun/article.c
+++ lib/kernel/lib/afun/article.c
@@ -1,7 +1,10 @@
 string article( string name ) {
   argcheck( name, 1, "string" );
 
-  if( lowercase( name ) == "hour" )
+  
+  if( (strlen(name) > 3 && lowercase( name[0..3] ) == "hour") ||
+      (strlen(name) > 4 && lowercase( name[0..4] ) == "honor" ) ||
+	  (strlen(name) > 5 && lowercase( name[0..5] ) == "honour" ) )
     return( "an" );
 
   switch( name[0] ) {
