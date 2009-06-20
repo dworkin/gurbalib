dc8144f4ea8ccdcad771cf96a4b19e50a77131c5
Bart van Leeuwen <aidil@wotf.org>
2009-06-20 22:39:31 +0000

    Change query_admin/wizard/mortal/user_priv to return 0 instead of throwing an error when the provided object argument is not a player object

--- lib/kernel/lib/afun-game/query_wizard.c
+++ lib/kernel/lib/afun-game/query_wizard.c
@@ -1,7 +1,9 @@
 int query_wizard(mixed player) {
 
   if(objectp(player)) {
-    argcheck( player<-"/sys/lib/player", 1, "player object or string" );
+    if( !player<-"/sys/lib/player" ) {
+      return 0;
+    }
     player = player->query_name();
   } else {
     argcheck( stringp(player), 1, "player object or string" );
