1825b1d4ce353e1cd020a8e05f7d2781537f8427
Bart van Leeuwen <aidil@wotf.org>
2009-09-26 14:54:32 +0000

    Added support for player age to the player object (patch by Thingol)

--- lib/std/body/heart_beat.c
+++ lib/std/body/heart_beat.c
@@ -1,6 +1,7 @@
 static int heal_rate;
 static int heal_amount;
 static int heal_time;
+int player_age;
 
 int    is_fighting( void );
 void   do_fight( void );
@@ -26,32 +27,32 @@ int query_heal_step( void ) {
   return( heal_amount );
 }
 
-void create() {
-   EVENT_D->subscribe_event("heart_beat");
-   heal_time = 0;
-   heal_rate = 10;
-   heal_amount = 2;
+void create()
+{
+  EVENT_D->subscribe_event("heart_beat");
+  heal_time = 0;
+  heal_rate = 10;
+  heal_amount = 2;
+  if( !intp( player_age ) ) {
+    player_age = 0;
+  }
 }
 
-void event_heart_beat( void )
-{
- object sp;
+void event_heart_beat( void ) {
+  object sp;
 
   sp = this_player();
   set_this_player(this_object());
 
-  if( this_object()->query_hp() < this_object()->query_max_hp() && this_object()->is_alive() )
-  {
+  if( this_object()->query_hp() < this_object()->query_max_hp() && this_object()->is_alive() ) {
     heal_time++;
-    if( heal_time > heal_rate )
-    {
+    if( heal_time > heal_rate ) {
         heal_time = 0;
         this_object()->increase_hp( random( heal_amount ) + 1 );
     }
   }
   /* Check here to see is we are in combat, if so, continue battle */
-  if( this_object()->is_fighting() )
-  {
+  if( this_object()->is_fighting() ) {
     this_object()->do_fight();
   } else {
     if( function_object( "event_wander", this_object()) ) {
@@ -61,9 +62,18 @@ void event_heart_beat( void )
 
   set_this_player(sp);
 
-  if( !this_object()->is_player() && this_object()->is_dead() )
-  {
-     EVENT_D->unsubscribe_event("heart_beat");
-     this_object()->destruct();
+  if(!this_object()->is_player())  /* NPC routines */ {
+    if(this_object()->is_dead()) {
+      EVENT_D->unsubscribe_event("heart_beat");
+      this_object()->destruct();
+    }
+  } else  /* Player routines */ {
+    if(this_object()->query_idle() < 60)  /* A player ages when not idle */
+    player_age += 1;
   }
+}
+
+
+nomask int query_age(varargs int raw) {
+  return player_age;
 }
