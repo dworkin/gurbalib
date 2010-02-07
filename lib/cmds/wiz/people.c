6f4996a8e4d0c5aeca8bbcdbb6f267b541066676
Bart van Leeuwen <aidil@wotf.org>
2010-02-07 12:13:37 +0000

    Fix bug in people command, can't get idletime on linkdead players

--- lib/cmds/wiz/people.c
+++ lib/cmds/wiz/people.c
@@ -1,13 +1,20 @@
 void main( string str ) {
   object *usr;
   int i;
+  mixed idle;
 
   usr = USER_D->query_users();
 
   for (i = 0; i < sizeof (usr); i++) {
-    write( capitalize(usr[i]->query_player()->query_name()) + "\t" + 
+    if( usr[i]->query_player() ) {
+      idle = usr[i]->query_player()->query_idle();
+    } else {
+      idle = 0;
+    }
+    write( capitalize(usr[i]->query_player()->query_name()) + "\t" +
            query_ip_number(usr[i]) + "\t" +
-           usr[i]->query_player()->query_environment()->query_brief() + " " + 
-           format_time(usr[i]->query_player()->query_idle()) + "\n");
+           usr[i]->query_player()->query_environment()->query_brief() + " " +
+           format_time( intp( idle ) ? idle : 0 ) + "\n");
   }
 }
+
