871e3ee0bca0f65cb0d23f382cf808c9cec0c488
Bart van Leeuwen <aidil@wotf.org>
2010-02-10 21:42:07 +0000

    Removed depricated learn, xgo, xget and xget2 commands

--- lib/cmds/player/learn.c
+++
@@ -1,9 +0,0 @@
-void main( string racialword ) {
-	if(racialword != "") {
-		string englishword;
-		sscanf(racialword, " ", racialword, englishword);
-		learn_racial_word(racialword, englishword);
-	} else {
-		write("Usage:\n  learn <foreignword> <englishword>\n");
-	}
-}
