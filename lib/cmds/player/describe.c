372371caeb646a6ed0956b29d6e7b0afa994d85c
Bart van Leeuwen <aidil@wotf.org>
2010-03-22 16:37:26 +0000

    Make  work for the describe command

--- lib/cmds/player/describe.c
+++ lib/cmds/player/describe.c
@@ -1,4 +1,8 @@
 void main( string str ) {
+  if( str ) {
+    str = replace_string( str, "$N", this_player()->query_proper_name() );
+  }
+
   this_player()->set_long( str );
   this_player()->simple_action( "$N $vchange $p looks." );
 }
