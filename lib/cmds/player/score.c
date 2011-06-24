2d87e13229397bf9bbf254602f076100872dd5a6
Kent Mein <mein@cs.umn.edu>
2011-06-23 20:29:35 -0500

    Combine stats, wealth and score command into one. Less to remember for players is better...
    Kent

--- lib/cmds/player/score.c
+++ lib/cmds/player/score.c
@@ -1,19 +1,57 @@
-int main( string str )
-{
-   int max_hp;
-   int hp;
-   int expr;
-   int ac;
+void usage () {
+   write("Usage: score [-h]\n");
+   write("Gives you basic stats about your character.\n");
+   write("Options:\n");
+   write("\t-h\tHelp, this usage message.\n");
+}
 
+int main( string str ) {
+   int max_hp, hp;
+   int expr;
+   int ac, i, ducats, royals, crowns;
+   mixed *money;
+   
+   money = this_player()->query_all_coins();
    max_hp = this_player()->query_max_hp();
    hp     = this_player()->query_hp();
    expr   = this_player()->query_expr();
    ac     = this_player()->query_defense();
 
-   write( "[ "+this_player()->query_name() + " ]" + "\n\n" );
-   write( "\t" + "hit points  :   " + hp + "/" + max_hp + "\n" );
-   write( "\t" + "experience  :   " + expr + "\n" );
-   write( "\t" + "armor class :   " + ac + "\n" );
+
+
+   write( "[ "+this_player()->query_name() + " ]" + "\n" );
+   write( "Strength    :  " + this_player()->query_stat("str") +
+	"\t\t" + "Hit points  :   " + hp + "/" + max_hp + "\n" );
+   write( "Intelligence:  " + this_player()->query_stat("int") +
+	"\t\t" + "Experience  :   " + expr + "\n" );
+   write( "Wisdom      :  " + this_player()->query_stat("wis") +
+	"\t\t" + "Armor class :   " + ac + "\n" );
+   if( sizeof( money ) == 0 ) {
+        write("Dexterity   :  " + this_player()->query_stat("dex")  +
+		"\t\tYou are broke.\n");
+	write("Constitution:  " + this_player()->query_stat("con") );
+        write("Charisma    :  " + this_player()->query_stat("cha") );
+
+   } else {
+      for( i = 0; i < sizeof( money ); i++ ) {
+         switch( money[i][0] ) {
+            case "ducat" : ducats = money[i][1];
+               break;
+            case "royal" : royals = money[i][1];
+               break;
+            case "crown" : crowns = money[i][1];
+               break;
+            default:
+               break;
+         }
+      }
+      write("Dexterity   : " + this_player()->query_stat("dex")  +
+      	 "\t\tDucats: " + ducats ); 
+      write("Constitution: " + this_player()->query_stat("con") +
+         "\t\tRoyals: " + royals ); 
+      write("Charisma    : " + this_player()->query_stat("cha") +
+         "\t\tCrowns: " + crowns ); 
+   }
    
    return 1;
 }
