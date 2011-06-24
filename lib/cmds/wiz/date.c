f6cf01344b82b22d4352b8dffb316ef870c6bb8c
Kent Mein <mein@cs.umn.edu>
2011-06-23 22:30:19 -0500

    Get usage working for date...
    Kent

--- lib/cmds/wiz/date.c
+++ lib/cmds/wiz/date.c
@@ -1,10 +1,14 @@
 void usage() {
   write("Usage: date [-h]\n");
-  write("Show the date.\n");
+  write("Show the realworld date.\n");
   write("Options:\n");
   write("\t-h\tHelp, this usage message.\n");
 }
 
 void main( string str ) {
+  if (str && str != "") {
+     usage();
+     return;
+  }
   write( ctime( time() ) + "\n" );
 }
