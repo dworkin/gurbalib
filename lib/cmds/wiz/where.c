b7b2f2d491601e73240578cdfe70239a3824ee22
Kent Mein <mein@cs.umn.edu>
2011-06-23 22:24:32 -0500

    Fixed where so it lets you ask the location of a specific person, and also made it so usage works.
    Kent

--- lib/cmds/wiz/where.c
+++ lib/cmds/wiz/where.c
@@ -1,6 +1,7 @@
 void usage() {
   write("Usage: where [-h] [player]\n");
   write("Locates the given [player] and prints out their location.\n");
+  write("If [player] is omitted it prints out everyones location.\n");
   write("Options:\n");
   write("\t-h\tHelp, this usage message.\n");
 }
@@ -8,13 +9,27 @@ void usage() {
 void main( string str ) {
   int i;
   object *usrs;
+  object usr;
 
-  usrs = USER_D->query_users();
+  if (!str || (str == "")) {
+    usrs = USER_D->query_users();
 
-  for( i = 0; i < sizeof( usrs ); i++ ) {
-    write( capitalize( usrs[i]->query_name() ) + " "
+    for( i = 0; i < sizeof( usrs ); i++ ) {
+      write( capitalize( usrs[i]->query_name() ) + " "
 	   + usrs[i]->query_player()->query_environment()->query_brief() + " "
 	   + usrs[i]->query_player()->query_environment()->base_name() + "\n"
 	   );
+    }
+  } else {
+     if (sscanf(str, "-%s",str)) {
+        usage();
+     } else if (usr = USER_D->find_user(str) ) {
+        write( capitalize( usr->query_name() ) + " "
+	   + usr->query_player()->query_environment()->query_brief() + " "
+	   + usr->query_player()->query_environment()->base_name() + "\n"
+	   );
+     } else {
+        write("Who are you looking for?  I can't find: " + str + "!\n");
+     }
   }
 }
