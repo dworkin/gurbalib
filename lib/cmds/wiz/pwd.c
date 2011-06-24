9329d486f695b54b329d66d31daaaf7a2b50ab82
Kent Mein <mein@cs.umn.edu>
2011-06-23 22:06:08 -0500

    added hook for usage to pwd
    Kent

--- lib/cmds/wiz/pwd.c
+++ lib/cmds/wiz/pwd.c
@@ -6,5 +6,9 @@ void usage() {
 }
 
 void main( string str ) {
-  write( "%^BOLD%^" + this_player()->query_env("pwd") + "%^RESET%^\n" );
+  if (str && (str != "")) {
+    usage();
+  } else {
+    write( "%^BOLD%^" + this_player()->query_env("pwd") + "%^RESET%^\n" );
+  }
 }
