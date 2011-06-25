955b7bfcff2d966dffcbe5250eac085ba9e2faeb
Kent Mein <mein@cs.umn.edu>
2011-06-25 11:01:40 -0500

    Made it so heal will allow you to heal other players.
    Kent

--- lib/cmds/wiz/heal.c
+++ lib/cmds/wiz/heal.c
@@ -1,14 +1,34 @@
 void usage() {
-  write("Usage: heal [-h]\n");
-  write("Allows you to heal yourself.\n");
+  write("Usage: heal [-h] [player]\n");
+  write("Allows you to heal a player, if no player is given heal yourself.\n");
   write("Options:\n");
   write("\t-h\tHelp, this usage message.\n");
 }
 
-void main( string str )
-{
- int max_hp;
- max_hp = this_player()->query_max_hp();
- this_player()->set_hp(max_hp);
- write("You are completely healed!\n");
+void main( string str ) {
+  int max_hp;
+  object obj;
+
+  if (str && str != "") {
+    obj = this_environment()->present(str);
+    if (!obj) {
+       write("heal who?\n");
+       return;
+    }
+  } else if (sscanf(str, "-%s", str) ) {
+    usage();
+    return;
+  } else {
+     obj = this_player();
+  }
+
+  max_hp = obj->query_max_hp();
+  obj->set_hp(max_hp);
+  this_player()->query_environment()->tell_room(this_player(),
+    capitalize(this_player()->query_name()) + " waves their hands in odd " +
+    "funny gestures.\n" +
+    capitalize(obj->query_name()) + " is completely healed!\n");
+  write("You wave your hands and heal " + capitalize(obj->query_name()) +
+    ".\n");
 }
+
