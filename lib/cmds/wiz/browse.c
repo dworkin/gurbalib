22e7bf4b57293e60f10ce2d1489b10b7e66733aa
Kent Mein <mein@cs.umn.edu>
2011-06-25 18:46:53 -0500

    Add usage to the browse command.
    Kent

--- lib/cmds/wiz/browse.c
+++ lib/cmds/wiz/browse.c
@@ -1,3 +1,15 @@
+void usage() {
+  write("Usage: browse [-h] [dir]\n");
+  write("An interactive way to look at files.");
+  write("Options:\n");
+  write("\t-h\tHelp, this usage message.\n");
+}
+
 void main(varargs string arg) {
+  if (sscanf(arg, "-%s",arg)) {
+     usage();
+     return;
+  }
+
   "/sys/obj/browser"->browse( arg );
 }
