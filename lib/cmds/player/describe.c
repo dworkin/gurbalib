03290f0314ae3e89bbe3305b65fe7753ac82a6b1
Kent Mein <mein@cs.umn.edu>
2011-06-26 02:37:36 -0500

    Another usage function.
    Kent

--- lib/cmds/player/describe.c
+++ lib/cmds/player/describe.c
@@ -1,4 +1,22 @@
+void usage() {
+  write("Usage: describe [-h] DESCRIPTION\n");
+  write("Allows you to change your DESCRIPTION, so that when someone looks " +
+    "at you,\nThey know what you look like.");
+  write("Options:\n");
+  write("\t-h\tHelp, this usage message.\n");
+}
+
 void main( string str ) {
+
+  if (!str || str == "") {
+    usage();
+    return;
+  }
+  if (sscanf(str, "-%s",str)) {
+    usage();
+    return;
+  }
+
   if( str ) {
     str = replace_string( str, "$N", this_player()->query_proper_name() );
   }
