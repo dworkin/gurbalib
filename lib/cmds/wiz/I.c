125e60169fe03bf7710fcdb144689f8e154d57b5
Kent Mein <mein@cs.umn.edu>
2011-06-26 13:17:14 -0500

    Added usage, not sure what this function is useful for yet but its there.
    Kent

--- lib/cmds/wiz/I.c
+++ lib/cmds/wiz/I.c
@@ -1,4 +1,22 @@
+void usage() {
+  write("Usage: I [-h] MSG\n");
+  write("Record a did MSG message, whatever that is....\n");
+  write("Options:\n");
+  write("\t-h\tHelp, this usage message.\n");
+}
+
+// XXX Figure out what this is used for... it stores an entry in 
+//     daemons/data/did_d.o   what the heck is it though????
 
 void main( string str ) {
+  if (!str || str == "") {
+     usage();
+     return;
+  }
+  if (sscanf(str, "-%s",str)) {
+     usage();
+     return;
+  }
+
   DID_D->add_entry( str );
 }
