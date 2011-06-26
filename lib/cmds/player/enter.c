0cf6b53fbb045c3aad42251f81ad72c775c9b3c1
Kent Mein <mein@cs.umn.edu>
2011-06-26 12:21:49 -0500

    A couple of additional usage functions.
    Kent

--- lib/cmds/player/enter.c
+++ lib/cmds/player/enter.c
@@ -1,9 +1,25 @@
-void main( string dir ) {
+void usage() {
+  write("Usage: enter [-h] OBJECT\n");
+  write("Allows you to enter the specified object.  To see what you\n");
+  write("can enter, \"look\" around.\n");
+  write("Options:\n");
+  write("\t-h\tHelp, this usage message.\n");
+}
+
+void main( string str ) {
   string error;
 
-  error = this_environment()->body_exit( this_player(), dir );
+  if ( !str || str == "" ) {
+    usage();
+    return;
+  }
+  if (sscanf(str, "-%s",str)) {
+     usage();
+     return;
+  }
 
-  if( error )
-    write( error );
+  error = this_environment()->body_exit( this_player(), str );
+
+  if( error ) write( error );
 }
 
--- lib/cmds/player/go.c
+++ lib/cmds/player/go.c
@@ -1,9 +1,25 @@
-void main( string dir ) {
+void usage() {
+  write("Usage: go [-h] DIRECTION\n");
+  write("Allows you to move in the specified direction.  To see where you\n");
+  write("can go, \"look\" around.\n");
+  write("Options:\n");
+  write("\t-h\tHelp, this usage message.\n");
+}
+
+void main( string str ) {
   string error;
 
-  error = this_environment()->body_exit( this_player(), dir );
+   if ( !str || str == "" ) {
+    usage();
+    return;
+  }
+  if (sscanf(str, "-%s",str)) {
+     usage();
+     return;
+  }
 
-  if( error )
-    write( error );
+  error = this_environment()->body_exit( this_player(), str );
+
+  if( error ) write( error );
 }
 
