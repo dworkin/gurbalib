00c95a40fe2725637b6fa858133359d0297d5251
Kent Mein <mein@cs.umn.edu>
2011-07-01 06:09:47 -0500

    Make it so the default workroom isn't so dark....
    Kent

--- lib/data/skel/rooms/workroom.c
+++ lib/data/skel/rooms/workroom.c
@@ -1,4 +1,3 @@
-
 inherit "/std/room";
 
 private string myname() {
@@ -7,20 +6,19 @@ private string myname() {
 
 void setup( void ) {
   set_brief( "%^RED%^" + myname() + "'s workroom%^RESET%^" );
-  set_long( "The room is splashed with gore. Piles of limbs in one corner"
-	    + " and piles of guts in another corner. In the middle of the room"
-	    + " is a desk. On the desk are some grotesque looking tools." );
+  set_long( "The room is littered with all kinds of arcane tools." +
+	    "Your not really sure what all this stuff is but someone looks " +
+            "pretty busy." );
   set_exits( ([ "out"  : "/domains/gurba/rooms/square_center",
 	        "east" : "/rooms/bug_room",
 	      ]) );
 
-  add_item( "desk", "The desk is a huge and made of mahogny. It's the central piece of furniture of the room, and it is clear that " + myname() + " spends a lot of time behind it." );
+  add_item( "desk", "The desk is a huge and made of mahogny. " +
+    "It's the central piece of furniture of the room, and it is clear that " + 
+    myname() + " spends a lot of time behind it." );
 
-  add_item( "tools", "They are used by grand master " + myname() + " to dissect monsters while trying to figure out how strong they are and where to place them." );
-
-  add_item( "gore", "Youck!" );
-
-  add_item( "limbs", "The ankle bone is connected to the leg bone. It's disgusting." );
+  add_item( "tools", "They are used by " + myname() + 
+    " to do all kinds of things." );
 
   set_objects ( ([
 		  "/domains/gurba/objects/chest" : 1,
