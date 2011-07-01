2233838ed222ad3de10cf1a069037c3a8e642a62
Kent Mein <mein@cs.umn.edu>
2011-07-01 08:32:51 -0500

    more usage work.
    Kent

--- lib/cmds/wiz/setenv.c
+++ lib/cmds/wiz/setenv.c
@@ -1,3 +1,11 @@
+void usage() {
+  write("Usage: setenv [-h] [VAR VALUE]\n");
+  write("Set an environment varilable VAR to VALUE.\n");
+  write("If no VAR VALUE pair is given print your current environment.\n");
+  write("Options:\n");
+  write("\t-h\tHelp, this usage message.\n");
+}
+
 void main( string str ) {
   string *names;
   string name;
@@ -10,17 +18,18 @@ void main( string str ) {
     for( i = 0; i < sizeof( names ); i++ ) {
       out_unmod( names[i] + "=" + this_player()->query_env( names[i] ) + "\n" ); 
     }
-  } else {
-    if( sscanf( str, "%s %s", name, value ) != 2 ) {
-      write( "Usage: set <variable> <value>\n" );
+  } else if (sscanf(str, "-%s",str)) {
+     usage();
+     return;
+  } else if( sscanf( str, "%s %s", name, value ) != 2 ) {
+      usage();
       return;
-    } else {
+  } else {
       if( str2val( value ) != -1 ) {
 	/* We've got a value */
 	this_player()->set_env( name, str2val(value) );
       } else {
 	this_player()->set_env( name, value );
       }
-    }
   }
 }
