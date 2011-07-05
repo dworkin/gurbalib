5b015c369b43186260ba86f52c9c82b338e6ff2c
Kent Mein <mein@cs.umn.edu>
2011-07-05 13:02:25 -0500

    Consolidate all of the ban code... Still need to work on unban for players but it's a start.
    Kent

--- lib/cmds/wiz/ban.c
+++ lib/cmds/wiz/ban.c
@@ -1,6 +1,8 @@
 void usage() {
   write("Usage: ban [-h] [PLAYERNAME|SITE]\n");
   write("Allows you to banish the specified player and or site.\n");
+  write("If no Player or SITE is given it displays what is currently " +
+    "banned.\n");
   write("Options:\n");
   write("\t-h\tHelp, this usage message.\n");
   write("Examples:\n");
@@ -8,10 +10,28 @@ void usage() {
   write("\tban 128.101.32.*\n");
 }
 
+void show_banned() {
+   string *sites, info;
+   int i;
+
+   write("Sites Banned:\n");
+   sites = SITEBAN_D->query_banned_sites();
+   for( i=0; i<sizeof(sites); i++) {
+      info = read_file("/data/banished/"+site);
+      write("\t" + sites[i] + "\t" + info +"\n");
+   }
+   write("Players Banned:\n");
+   sites = SITEBAN_D->query_banished_names();
+   for( i=0; i<sizeof(sites); i++) {
+      info = read_file("/data/banished/"+site);
+      write("\t" + sites[i] + "\t" + info + "\n");
+   }
+}
+
 void main( string str ) {
    if(!str || str == "") {
-      usage();
-      return;
+     show_banned();
+     return;
    }
    if (sscanf(str, "-%s",str)) {
      usage();
@@ -32,6 +52,8 @@ void main( string str ) {
       if (query_admin(str) && !query_admin(this_player->query_name()) {
          write("Wizards are not allowed to ban an admin.\n");
          return;
+      } else if (this_player()->query_name() == str) {
+         write("You may not ban yourself.\n);
       } else {
          if( BANISH_D->banish_name( str ) ) {
             this_player()->simple_action( "$N $vbanish "+str+".\n" );
--- lib/cmds/wiz/unban.c
+++ lib/cmds/wiz/unban.c
@@ -1,8 +1,45 @@
+void usage() {
+  write("Usage: ban [-h] [PLAYERNAME|SITE]\n");
+  write("Allows you to banish the specified player and or site.\n");
+  write("If no Player or SITE is given it displays what is currently " +
+    "banned.\n");
+  write("Options:\n");
+  write("\t-h\tHelp, this usage message.\n");
+  write("Examples:\n");
+  write("\tban sirdude\n");
+  write("\tban 128.101.32.*\n");
+}
+
+void show_banned() {
+   string *sites, info;
+   int i;
+
+   write("Sites Banned:\n");
+   sites = SITEBAN_D->query_banned_sites();
+   for( i=0; i<sizeof(sites); i++) {
+      info = read_file("/data/banished/"+site);
+      write("\t" + sites[i] + "\t" + info +"\n");
+   }
+   write("Players Banned:\n");
+   sites = SITEBAN_D->query_banished_names();
+   for( i=0; i<sizeof(sites); i++) {
+      info = read_file("/data/banished/"+site);
+      write("\t" + sites[i] + "\t" + info + "\n");
+   }
+}
+
 void main( string str ) {
-   if(!str) {
-      write("Unban which site? ( Format: *.*.*.* )\n");
+   if(!str || str == "") {
+      show_banned();
       return;
    }
+   if (sscanf(str, "-%s",str)) {
+     usage();
+     return;
+   }
+
    SITEBAN_D->unsiteban(str);
    this_player()->simple_action( "$N $vunban "+str+"\n" );
+
+// XXX  Need to add logic for player unban as well....
 }
