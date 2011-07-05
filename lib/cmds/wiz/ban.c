1a05c199b93a62f462acfe41b612794d2f2a608b
Kent Mein <mein@cs.umn.edu>
2011-07-05 09:59:04 -0500

    Work on unifying ban commands...
    Kent

--- lib/cmds/wiz/ban.c
+++ lib/cmds/wiz/ban.c
@@ -1,24 +1,43 @@
-void main( string str ) {
-   if(!str) {
-      write("Ban which site? ( example: 1.2.*.* )\n");
-      return;
-   }
-/*
-   if( str == "35.8.*.*" || str == "152.*.*.*" ) {
-      write("Ban "+str+"?!? You must be kidding...\n");
-      return;
-   }
-*/
-#ifndef ALLOW_SHOOTING_ONESELF
-  if( str == "127.*.*.*" || str == "127.0.*.*" || str == "127.0.0.*" || str == "127.0.0.1" ) {
-      write("Ban "+str+"?!? You must be kidding...\n");
-      return;
-   }
-#endif
+void usage() {
+  write("Usage: ban [-h] [PLAYERNAME|SITE]\n");
+  write("Allows you to banish the specified player and or site.\n");
+  write("Options:\n");
+  write("\t-h\tHelp, this usage message.\n");
+  write("Examples:\n");
+  write("\tban sirdude\n");
+  write("\tban 128.101.32.*\n");
+}
 
+void main( string str ) {
+   if(!str || str == "") {
+      usage();
+      return;
+   }
+   if (sscanf(str, "-%s",str)) {
+     usage();
+     return;
+   }
+   if( str == "127.*.*.*" || str == "127.0.*.*" || str == "127.0.0.*" || 
+      str == "127.0.0.1" ) {
+      write("Ban "+str+"?!? You must be kidding...\n");
+      return;
+   }
+   if( strstr( str, " " ) != -1 ) {
+      write("Cannot include whitespace in banished names.");
+      return;
+   }
    if( SITEBAN_D->siteban(str) ) {
       this_player()->simple_action( "$N $vban the site: "+str+"\n" );
    } else {
-      write("Bad ban format, please try again.\n");
+      if (query_admin(str) && !query_admin(this_player->query_name()) {
+         write("Wizards are not allowed to ban an admin.\n");
+         return;
+      } else {
+         if( BANISH_D->banish_name( str ) ) {
+            this_player()->simple_action( "$N $vbanish "+str+".\n" );
+         } else {
+            usage();
+         }
+      }
    }
 }
