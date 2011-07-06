259e6292449e447b340ae351cc6223528d51c077
Kent Mein <mein@cs.umn.edu>
2011-07-06 10:04:09 -0500

    Updated the cmds command so it prints commands via section. Probably still needs some tidying up...
    Kent

--- lib/cmds/wiz/cmds.c
+++ lib/cmds/wiz/cmds.c
@@ -13,51 +13,48 @@ void main( string str ) {
   mapping cmds;
   int c, sz, col;
 
-  if (sscanf(str, "-%s",str)) {
-     usage();
-     return;
+  if( str == "-v" ) {
+    col = 1;
+  } else {
+    col = 4;
+    if (str && str != "") {
+       usage();
+       return;
+    }
   }
-
-  cmds = ([ ]);
 
   path = this_player()->query_path();
-
-  if(!query_wizard( this_player() ) ) {
-    str = "";
-  }
 
   for( c = sizeof( path ) - 1; c >= 0; c-- ) {
     string * files;
     int i;
 
+    cmds = ([ ]);
     files = get_dir( path[c] + "/*.c" )[0];
     for( i = sizeof( files ) - 1; i >= 0; i-- ) {
       cmds[files[i][..strlen(files[i])-3]] = path[c];
     }
-  }
 
-  names = map_indices( cmds );
-  if( str == "-v" ) {
-    col = 1;
-  } else {
-    col = 4;
-  }
+    names = map_indices( cmds );
 
-  for( c = 0, sz = sizeof( names ); c < sz; c += col ) {
-    int colnum;
-    string line;
+    write ("Commands: " + path[c] + "\n");
 
-    line = "";
+    for( c = 0, sz = sizeof( names ); c < sz; c += col ) {
+      int colnum;
+      string line;
 
-    for( colnum = 0; colnum < col; colnum++ ) {
-      if( c + colnum < sz ) {
-        line += (
-          names[c + colnum] + 
-          ( str == "-v" ? " (" + cmds[names[c+colnum]] + ")" : "" ) +
-          "                                                                      ")[0..(70/col)];
+      line = "";
+
+      for( colnum = 0; colnum < col; colnum++ ) {
+        if( c + colnum < sz ) {
+          line += ( names[c + colnum] + 
+             ( str == "-v" ? " (" + cmds[names[c+colnum]] + ")" : "" ) +
+             "                                             " +
+             "                         ")[0..(70/col)];
+        }
       }
+      write( line );
     }
-    write( line );
   }
 }
       
