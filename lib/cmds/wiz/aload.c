5d81f601e1f3068b645c61f3e40bfffc1d9eed6b
Kent Mein <mein@cs.umn.edu>
2011-07-06 17:39:48 -0500

    More usage work...
    Kent

--- lib/cmds/wiz/aload.c
+++ lib/cmds/wiz/aload.c
@@ -1,7 +1,25 @@
+void usage() {
+  write("Usage: aload [-h] [FILE]\n");
+  write("Not sure how this works yet....XXX\n");
+  write("If no FILE is given display current autoload string.\n");
+  write("Options:\n");
+  write("\t-h\tHelp, this usage message.\n");
+  write("See also: compose???\n");
+}
+
 void main( string str ) {
    string file, argument, rest;
    object ob;
-   
+
+   if ( !str || str == "" ) {
+      str = this_player()->query_autoload_string();
+      write ("Your autoload_string is: "+str+"\n");
+   }
+   if (sscanf(str, "-%s",str)) {
+      usage();
+      return;
+   }
+
    str = this_player()->query_autoload_string();
    while(str && str != "*^!") {
       if (sscanf(str, "%s:%s^!%s", file, argument, rest) != 3) {
