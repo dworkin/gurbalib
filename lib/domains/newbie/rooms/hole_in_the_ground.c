58d725a98dc50eab3d2c924a939b84df23b23a43
Kent Mein <mein@cs.umn.edu>
2011-07-14 11:07:25 -0500

    Forgot this little change in last commit.
    Kent

--- lib/domains/newbie/rooms/hole_in_the_ground.c
+++ lib/domains/newbie/rooms/hole_in_the_ground.c
@@ -1,11 +1,16 @@
 inherit ROOM;
 
 void setup( void ) {
-  set_brief( "%^MAGENTA%^North end of field.%^RESET%^" );
-  set_long( "You are in a large open field.  To the South you see a " +
-  "road leading to a city.  There is an odd hole in a hill to the north.");
-  set_exits( ([ 
-	       "south" : "/domains/required/rooms/start",
-  ]) );
+   set_brief( "%^MAGENTA%^North end of field.%^RESET%^" );
+   set_long( "You are in a large open field.  To the South you see a " +
+   "road leading to a city.  There is an odd hole in a hill to the north.");
+   set_exits( ([ 
+      "south" : "/domains/required/rooms/start",
+   ]) );
+   set_objects( ([ 
+      "/domains/required/objects/sign" : 1,
+   ]) );
+   add_item("hole", "A funny hole in the ground.  You might be able to " +
+      "enter it.");
 }
 
