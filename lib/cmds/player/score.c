eb42d9dc310d978e84fbe2ec389208703b92f45a
Kent Mein <mein@cs.umn.edu>
2011-07-14 04:22:28 -0500

    Add level to score and make it so mud will increase your level...
    Kent

--- lib/cmds/player/score.c
+++ lib/cmds/player/score.c
@@ -1,62 +1,75 @@
 void usage () {
-   write("Usage: score [-h]\n");
-   write("Gives you basic stats about your character.\n");
-   write("Options:\n");
-   write("\t-h\tHelp, this usage message.\n");
+   if(query_wizard( this_player() ) ) {
+      write("Usage: score [-h] [PLAYER]\n");
+      write("Gives you basic stats about player PLAYER.\n");
+      write("If no player is given give your stats.\n");
+      write("Options:\n");
+      write("\t-h\tHelp, this usage message.\n");
+   } else {
+      write("Usage: score [-h]\n");
+      write("Gives you basic stats about your character.\n");
+      write("Options:\n");
+      write("\t-h\tHelp, this usage message.\n");
+   }
 }
 
 void main( string str ) {
-   int max_hp, hp;
-   int expr;
+   int max_hp, hp, expr, level;
    int ac, i, ducats, royals, crowns;
    mixed *money;
+   object obj;
+
+   if (sscanf(str, "-%s",str)) {
+     usage();
+     return;
+   }
 
    if (str && str != "") {
-      usage();
-      return;
+      obj = USER_D->find_player(str);
+      if (!obj) {
+         write("Unable to find player: " + str + "\n");
+         return;
+      }
+   } else {
+      obj = this_player();
    }
    
-   money = this_player()->query_all_coins();
-   max_hp = this_player()->query_max_hp();
-   hp     = this_player()->query_hp();
-   expr   = this_player()->query_expr();
-   ac     = this_player()->query_defense();
+   money = obj->query_all_coins();
+   max_hp = obj->query_max_hp();
+   hp     = obj->query_hp();
+   expr   = obj->query_expr();
+   ac     = obj->query_defense();
+   level  = obj->query_level();
 
-
-
-   write( "[ "+this_player()->query_name() + " ]" + "\n" );
-   write( "Strength    :  " + this_player()->query_stat("str") +
+   write( "[ " + obj->query_name() + " ]" + "\n" );
+   write( "Strength    :  " + obj->query_stat("str") +
 	"\t\t" + "Hit points  :   " + hp + "/" + max_hp + "\n" );
-   write( "Intelligence:  " + this_player()->query_stat("int") +
+   write( "Intelligence:  " + obj->query_stat("int") +
 	"\t\t" + "Experience  :   " + expr + "\n" );
-   write( "Wisdom      :  " + this_player()->query_stat("wis") +
+   write( "Wisdom      :  " + obj->query_stat("wis") +
+	"\t\t" + "Level       :   " + level + "\n" );
+   write( "Dexterity   :  " + obj->query_stat("dex") +
 	"\t\t" + "Armor class :   " + ac + "\n" );
-   if( sizeof( money ) == 0 ) {
-        write("Dexterity   :  " + this_player()->query_stat("dex")  +
-		"\t\tYou are broke.\n");
-	write("Constitution:  " + this_player()->query_stat("con") );
-        write("Charisma    :  " + this_player()->query_stat("cha") );
+   write("Constitution:  " + obj->query_stat("con") );
+   write("Charisma    :  " + obj->query_stat("cha") +
+        "\t\t" + "Money:\n" );
 
-   } else {
-      for( i = 0; i < sizeof( money ); i++ ) {
-         switch( money[i][0] ) {
-            case "ducat" : ducats = money[i][1];
-               break;
-            case "royal" : royals = money[i][1];
-               break;
-            case "crown" : crowns = money[i][1];
-               break;
-            default:
-               break;
+   for( i = 0; i < sizeof( money ); i++ ) {
+      switch( money[i][0] ) {
+         case "ducat" : ducats = money[i][1];
+            break;
+         case "royal" : royals = money[i][1];
+            break;
+         case "crown" : crowns = money[i][1];
+            break;
+         default:
+            break;
          }
-      }
-      write("Dexterity   : " + this_player()->query_stat("dex")  +
-      	 "\t\tDucats: " + ducats ); 
-      write("Constitution: " + this_player()->query_stat("con") +
-         "\t\tRoyals: " + royals ); 
-      write("Charisma    : " + this_player()->query_stat("cha") +
-         "\t\tCrowns: " + crowns ); 
    }
+   write("\t\t\t\t\tDucats: " + ducats ); 
+   write("\t\t\t\t\tRoyals: " + royals ); 
+   write("\t\t\t\t\tCrowns: " + crowns ); 
    
    return;
 }
+
--- lib/std/body/experience.c
+++ lib/std/body/experience.c
@@ -1,6 +1,23 @@
 int experience;
+int level;
 
 void create( void ) {
+}
+
+int ready_for_next_level() {
+   int tmpexp;
+
+   tmpexp = level * level * 2000;
+
+   if (experience > tmpexp) {
+      return 1;
+   }
+   return 0;
+}
+
+void increase_level() {
+   level += 1;
+   write("Congragulations, you just achieved level: " + level + "\n");
 }
 
 void increase_expr( int expr ) {
@@ -11,7 +28,11 @@ void increase_expr( int expr ) {
    if( experience < 0 ) {
       experience = 0;
    }
-
+   if (ready_for_next_level()) {
+	increase_level();
+	write("Congragulations, you just went up a level...\n");
+	level += 1;
+   }
 }
 
 void decrease_expr( int expr ) {
@@ -31,14 +52,12 @@ void set_expr( int expr ) {
    }
 }
 
-void add_expr( int expr ) {
-   experience += expr;
-   if( experience < 0 ) {
-      experience = 0;
-   }
-}
-
-
 int query_expr( void ) {
   return( experience );
 }
+
+int query_level(void) {
+   if (level < 1) level = 1;
+   return( level );
+}
+
