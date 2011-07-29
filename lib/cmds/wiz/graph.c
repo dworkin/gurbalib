3e3a7d23d4da597ffe8ce85b4b689087ada51f0e
Kent Mein <mein@cs.umn.edu>
2011-07-29 15:46:32 -0500

    More work on getting this working...
    Kent

--- lib/cmds/wiz/graph.c
+++ lib/cmds/wiz/graph.c
@@ -6,6 +6,36 @@ void usage() {
   write("\t-h\tHelp, this usage message.\n");
 }
 
+void print_node(string room, string dir) {
+   mapping *exits;
+   string *keys, filepath;
+   int i,maxi,len;
+   object obj;
+
+   obj = find_object(dir + "/" + room);
+
+// XXX maybe need to do more work here to load the room?
+
+   if (obj) {
+      len = strlen(dir);
+      exits = obj->query_exits();
+      keys = obj->query_exit_indices();
+      if (exits) {
+         maxi = sizeof(keys);
+         for(i=0;i<maxi;i++) {
+            if (0) {  // XXX check to see if front matches dir if so cut it ...
+               filepath = exits[keys[i]];
+            } else {
+               filepath = exits[keys[i]];
+            }
+            write("\t" + keys[i] + " -> " + filepath + ";\n");
+         }
+      }
+   } else {
+      write("# Failed to load " + $dir + "/" + room + "\n");
+   }
+}
+
 void main( string str ) {
   int i;
 
@@ -43,11 +73,14 @@ void main( string str ) {
   }
 
   /* do the work here... */
-  /* XXX */ 
   i = sizeof(files);
   while(i>0) {
      i--;
-     write("Looking at : " + str + "/" + files[i] + "\n");
+     write("# Looking at : " + str + "/" + files[i] + "\n");
+     write("digraph G {\n");
+     print_node(files[i],str);
+     write("}\n");
+     write("# End: str\n");
   }
 }
 
