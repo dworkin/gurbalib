0b701ba1588322789f9bd136b005a85875d5624e
Kent Mein <mein@cs.umn.edu>
2011-08-08 12:01:26 -0500

    Add a hint to read a sign when you see one.
    Kent

--- lib/std/sign.c
+++ lib/std/sign.c
@@ -7,7 +7,7 @@ inherit M_READABLE;
 void create( void ) {
   ::create( );
   set_id( "sign" );
-  set_long( "Your average wooden sign." );
+  set_long( "Your average wooden sign.  It has some writing on it." );
   set_in_room_desc( "A wooden sign." );
   set_gettable( 0 );
 }
