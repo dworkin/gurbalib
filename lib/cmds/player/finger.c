fb7f35162cb5bd0921d73952823b2532e7c7e68a
Kent Mein <mein@cs.umn.edu>
2011-08-28 21:57:20 -0500

    Added a todo and modified the finger command some more to make it usable by all players....  It just gives different info depending on who you are...
    Kent

--- lib/cmds/player/finger.c
+++ lib/cmds/player/finger.c
@@ -5,6 +5,41 @@ void usage() {
   write("\t-h\tHelp, this usage message.\n");
 }
 
+void print_info(object player, int found, int cloned) {
+
+  if (found == 1) {
+    write( "%^BLUE%^Name:%^RESET%^ " + capitalize(player->query_name()) + 
+       "\n");
+    write( "%^BLUE%^Title:%^RESET%^ " + player->query_title() + "\n");
+    if ( query_admin(player->query_name()) == 1 ) { 
+      write( "%^BLUE%^Status: %^RESET%^Administrator\n");
+    } else if( query_wizard( player->query_name() ) == 1 ) {
+      write( "%^BLUE%^Status: %^RESET%^Wizard\n");
+    } else {
+      write( "%^BLUE%^Status: %^RESET%^Player\n");
+    }
+  }
+
+  if (query_wizard(this_player())) {
+     write( "%^BLUE%^Description:%^RESET%^ " + player->query_long() + "\n");
+  }
+
+  if (query_admin(this_player())) {
+     write( "%^BLUE%^Real name: %^RESET%^" + player->query_real_name()  + "\n");
+     write( "%^BLUE%^Email address: %^RESET%^" + 
+        player->query_email_address() + "\n");
+  }
+
+  if (player->query_idle() > 60) {
+    write( "%^BLUE%^Idle: %^RESET%^" + format_time(player->query_idle()) + 
+      "\n");
+  }
+
+  if (cloned == 1) {
+    write( "%^BLUE%^Last login: %^RESET%^" + ctime(player->query_last_login()));
+  }
+}
+
 void main( string str) {
   object *players;
   int i;
@@ -16,6 +51,7 @@ void main( string str) {
      usage();
      return;
   }
+
   if (sscanf(str, "-%s",str)) {
      usage();
      return;
@@ -32,6 +68,9 @@ void main( string str) {
 	found = 1;
     }
   }
+
+  // XXX need to move this to userd or something so that players can call 
+  // finger.
   if (found != 1 && file_exists( "/data/players/" + str + ".o")) {
     write( "%^RED%^" + capitalize(str) + " is not logged in.%^RESET%^");
     player = clone_object( PLAYER_OB );
@@ -41,29 +80,7 @@ void main( string str) {
     cloned = 1;
   }    
     
-  if (found == 1) {
-    write( "%^BLUE%^Name:%^RESET%^ " + capitalize(player->query_name()) + 
-       ".\n");
-    write( "%^BLUE%^Title:%^RESET%^ " + player->query_title() + ".\n");
-    write( "%^BLUE%^Description:%^RESET%^ " + player->query_long() + "\n");
-    if ( query_admin(player->query_name()) == 1 ) { 
-      write( "%^BLUE%^Status: %^RESET%^Administrator\n");
-    } else if( query_wizard( player->query_name() ) == 1 ) {
-      write( "%^BLUE%^Status: %^RESET%^Wizard\n");
-    } else {
-      write( "%^BLUE%^Status: %^RESET%^Player\n");
-    }
-  }
-  write( "%^BLUE%^Real name: %^RESET%^" + player->query_real_name()  + "\n");
-  write( "%^BLUE%^Email address: %^RESET%^" + player->query_email_address() + "\n");
-  if (player->query_idle() > 60) {
-    write( "%^BLUE%^Idle: %^RESET%^" + format_time(player->query_idle()) + 
-      "\n");
-  }
-
-  if (cloned == 1) {
-    write( "%^BLUE%^Last login: %^RESET%^" + ctime(player->query_last_login()));
-  }
+  print_info(player, found, cloned);
     
   if ((player->query_name() != this_player()->query_name()) && cloned != 1) {
     player->message( "You feel like someone is thinking about you.\n");
