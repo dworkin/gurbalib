90df6c1595342902125482a0b0d29c457e9feb44
Kent Mein <mein@cs.umn.edu>
2011-09-14 19:53:15 -0500

    Rename biff to forcequit command...
    Kent

--- lib/cmds/wiz/forcequit.c
+++ lib/cmds/wiz/forcequit.c
@@ -1,5 +1,5 @@
 void usage() {
-  write("Usage: biff [-h] PLAYER\n");
+  write("Usage: forcequit [-h] PLAYER\n");
   write("Locates the given PLAYER and forces them to quit the game.\n");
   write("Options:\n");
   write("\t-h\tHelp, this usage message.\n");
@@ -20,7 +20,7 @@ void main( string str ) {
   ob = USER_D->find_player( str );
   if( ob ) {
       if( ob->is_player() == 1) {
-         this_player()->simple_action( "$N $vbiff "+ob->query_name()+
+         this_player()->simple_action( "$N $vforce "+ ob->query_name() +
            " off the game.\n" );
          ob->do_quit();
       }
