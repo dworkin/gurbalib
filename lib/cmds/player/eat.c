413e5997d6bbb86662f6b37664eb3dede4fee4b4
Kent Mein <mein@cs.umn.edu>
2012-01-01 19:03:07 -0600

    Removed spuds workroom, its just the default basically...
    Added a new command eat.  It still needs support code done.
    
    Kent

---
+++ lib/cmds/player/eat.c
@@ -0,0 +1,34 @@
+void usage() {
+   write("Usage: eat [-h] [OBJECT]\n");
+   write("Consume the specified object.\n");
+   write("Options:\n");
+   write("\t-h\tHelp, this usage message.\n");
+}
+
+void main( string str ) {
+  object obj;
+
+  if (str && str != "") {
+     usage();
+     return;
+  }
+
+  if (sscanf(str, "-%s", str)) {
+     usage();
+     return;
+  }
+
+  obj = this_player()->present( lowercase (str) );
+  if (!obj ) {
+     write("Consume what?\n");
+     return;
+  }
+  
+  if ( !obj->is_edable() ) {
+     write(" You can not consume that.\n");
+     return;
+  }
+
+  call_other(obj,"do_eat");
+}
+
