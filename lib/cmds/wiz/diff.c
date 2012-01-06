688204b2b923767506fd73599d2a6dc0610e550e
Kent Mein <mein@cs.umn.edu>
2012-01-05 23:40:38 -0600

    Did a little work on the errors for usage on diff.  It still needs the actual code though.
    Kent

--- lib/cmds/wiz/diff.c
+++ lib/cmds/wiz/diff.c
@@ -8,6 +8,26 @@ void usage() {
   write("See also: grep\n");
 }
 
+// XXX Still need to do more here...
+void do_work_tofile(string outfile, string file1, string file2) {
+}
+
+void do_work_toscreen(string file1, string file2) {
+}
+
 void main( string str ) {
-   // XXX do the work here...
+   string outfile, file1, file2);
+   if( !str || str == "" || sscanf( str, "-h", str)) {
+      return usage();
+   }  
+
+   if (sscanf(str, "-o %s %s %s",outfile,file1,file2)) {
+      do_work_tofile(outfile,file1,file2);
+   } else {
+      if sscanf(str, "%s %s", file1, file2) {
+         do_work_toscreen(file1,file2);
+      } else {
+         return usage();
+      }
+   }
 }
