7dbdf8b6aa4952a6c6b6e7c9b92b049a924432cc
Kent Mein <mein@cs.umn.edu>
2012-01-06 00:40:01 -0600

    Added a consumeable...  It still needs work though.
    Kent

---
+++ lib/domains/newbie/objects/potion.c
@@ -0,0 +1,20 @@
+inherit OBJECT;
+inherit M_EATABLE;
+
+// XXX Need to finish this and actually put it in a room...
+
+void setup( void ) {
+  set_id( "potion" );
+  set_short("potion of healing");
+  set_long( "A red potion in a fancy bottle.\n");
+  set_edable(1);
+}
+
+int do_eat( void ) {
+  int x;
+
+  x = random(5) + 1;
+  write("You drink the potion.  You feel a warmth in your chest.\n");
+  this_player()->increase_hp(x);
+  this_object()->destruct();
+}
