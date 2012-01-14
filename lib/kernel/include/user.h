d1ccb6e1075617b5042304271fd86b30734a1ac0
Kent Mein <mein@cs.umn.edu>
2012-01-14 10:21:47 -0600

    Removed duplicate files maybe still needs some cleaning up. Will check it after this commit.
    Kent

--- lib/kernel/include/user.h
+++ lib/kernel/include/user.h
@@ -3,3 +3,4 @@ void input_to_object( object ob, string func );
 void write( string str );
 void login();
 void get_name( string str );
+void input_name( string str );
--- lib/std/include/user.h
+++
@@ -1,6 +0,0 @@
-void input_to( string func );
-void input_to_object( object ob, string func );
-void write( string str );
-void login();
-void input_name( string str );
-
