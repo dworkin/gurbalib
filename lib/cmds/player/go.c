f5b23a7794cc24a32f7e78f7c5d5d4b442dfd28a
Kent Mein <mein@cs.umn.edu>
2012-01-30 22:37:33 -0600

    Updated the global aliases adding a drink alias that points to the eat command. Also updated go command so that n -h will give usage for go...
    I also added more todo items...
    
    Kent

--- lib/cmds/player/go.c
+++ lib/cmds/player/go.c
@@ -8,15 +8,20 @@ void usage() {
 
 void main( string str ) {
   string error;
+  string str2;
+  int len;
 
    if ( !str || str == "" ) {
     usage();
     return;
   }
-  if (sscanf(str, "-%s",str)) {
+  if (sscanf(str, "-%s",str) || sscanf(str, "%s -%s",str,str2)) {
      usage();
      return;
   }
+
+  len = strlen(str);
+  if (str[len-1] == ' ') str = str[..len-2];
 
   error = this_environment()->body_exit( this_player(), str );
 
