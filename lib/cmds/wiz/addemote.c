83eebda2cafa8637e3ee0b4ee165ac0d8472ad02
Kent Mein <mein@cs.umn.edu>
2012-02-19 14:02:42 -0600

    Did some work on the usage for addemote
    Kent

--- lib/cmds/wiz/addemote.c
+++ lib/cmds/wiz/addemote.c
@@ -1,12 +1,25 @@
 void usage() {
-  write("Usage: addemote [-h] NAME RULE FORMAT\n");
+  write("Usage: addemote [-h] NAME [RULE] FORMAT\n");
   write("Add an emote to the emote database.\n");
+  write("Name is the thing players will type for the emote.\n");
+  write("Rule if provided is one of the following: LIV|STR|OBJ\n");
+  write("It only applies if the target of your emote is of that type.\n");
+  write("For emotes without a target, you do not provide a rule.\n");
+  write("Format defines what the emote will do.\n");
+  write("Example: if you do addemote blink $N blink.\n");
+  write("\tYou will see You blink.\n");
+  write("\t everyone else will see Sirdude blink. (If your name is sirdude)\n");
 
-// XXX Need to flesh out help here.... what is RULE, what is FORMAT?
+// XXX Need to flesh out help here.... what are the special symbols...
 
+  write("Examples:\n");
+  write("\taddemote blink $N blink.\n");
+  write("\taddemote LIV $N $vblink at $t\n");
   write("Options:\n");
   write("\t-h\tHelp, this usage message.\n");
 }
+
+// XXX Need to mabye combine this will showemote and rmemote
 
 void main( string str ) {
   string name;
