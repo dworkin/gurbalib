3a09e9dd6c4248c1297049288ba1353c657a6154
Kent Mein <mein@cs.umn.edu>
2012-02-27 21:44:28 -0600

    Fix wield message and add an alias for branch for the stick.
    Kent

--- lib/domains/newbie/objects/stick.c
+++ lib/domains/newbie/objects/stick.c
@@ -2,6 +2,7 @@ inherit WEAPON;
 
 void setup( void ) {
   set_id( "stick" );
+  add_id( "branch" );
   set_adj( "short" );
   set_long( "A rough branch from a tree." );
   set_in_room_desc( "A stick." );
@@ -14,7 +15,7 @@ void setup( void ) {
   set_weapon_skill( "combat/blunt/small" );
 
   set_wield_type( "single" );
-  set_wield_message( "$N $vgrab $p $o." );
+  set_wield_message( "$N $vgrab $o." );
   set_unwield_message( "$N $vloosen $p $vgrip on $o, and $vput it away." );
   set_weapon_action("bash");
   set_value( 1 );
