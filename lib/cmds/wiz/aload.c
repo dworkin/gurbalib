bf078ade4b7024e36c49ccd32f43cd4ad4059294
Kent Mein <mein@cs.umn.edu>
2012-03-01 16:52:44 -0600

    Update usage for aload and compose. Not really sure if we really need/want these two but hey... Will look at it more later.
    Kent

--- lib/cmds/wiz/aload.c
+++ lib/cmds/wiz/aload.c
@@ -1,10 +1,10 @@
 void usage() {
   write("Usage: aload [-h] [FILE]\n");
-  write("Not sure how this works yet....XXX\n");
-  write("If no FILE is given display current autoload string.\n");
+  write("Auto load your current autoload string.\n");
+  write("XXX not really used right now, need to look at it more.\n");
   write("Options:\n");
   write("\t-h\tHelp, this usage message.\n");
-  write("See also: compose???\n");
+  write("See also: compose\n");
 }
 
 void main( string str ) {
@@ -12,15 +12,14 @@ void main( string str ) {
    object ob;
 
    if ( !str || str == "" ) {
-      str = this_player()->query_autoload_string();
       write ("Your autoload_string is: "+str+"\n");
-   }
-   if (sscanf(str, "-%s",str)) {
+   } else {
       usage();
       return;
    }
 
    str = this_player()->query_autoload_string();
+
    while(str && str != "*^!") {
       if (sscanf(str, "%s:%s^!%s", file, argument, rest) != 3) {
          write("Autoload string corrupt.\n");
--- lib/cmds/wiz/compose.c
+++ lib/cmds/wiz/compose.c
@@ -1,6 +1,14 @@
 void usage () {
-	write("XXXX");
+   write("Usage compose [-h]");
+   write("Scan the objects in your inventory and create an autoload string.\n");
+   write("Options:\n");
+   write("\t-h\tHelp, this usage message.\n");
+   write("See also: aload\n");
 }
+
+// XXX I'm assuming this needs work still.  Currently autoloading is not used.
+// look at std/m_autoload_filename.c and std/modules/m_autoload_filename.c
+//  for more info, also why both???  should be in one location...
 
 void main( string str ) {
    object ob;
