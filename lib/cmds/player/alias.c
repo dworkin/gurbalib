392239d69ecdc20b4cc7fe5bed42205f5488b650
Kent Mein <mein@cs.umn.edu>
2012-03-22 16:42:21 -0500

    Added a script to standardize code format
    Changed the indentation on alias.c and bug.c also as tests.
    
    Kent

--- lib/cmds/player/alias.c
+++ lib/cmds/player/alias.c
@@ -1,56 +1,56 @@
 void usage() {
-  write("Usage: alias [-h] [STR COMMAND]\n");
-  write("Allows you to create an alias for a command.\n");
-  write("If no alias is given show a list of your current aliases.\n");
-  write("Options:\n");
-  write("\t-h\tHelp, this usage message.\n");
-  write("Example:\n");
-  write("\talias ga get all\n");
+   write("Usage: alias [-h] [STR COMMAND]\n");
+   write("Allows you to create an alias for a command.\n");
+   write("If no alias is given show a list of your current aliases.\n");
+   write("Options:\n");
+   write("\t-h\tHelp, this usage message.\n");
+   write("Example:\n");
+   write("\talias ga get all\n");
 }
 
-void main( string str ) {
-  mapping alias;
-  string *aliases;
-  string cmd;
-  string arg;
-  int i;
+void main(string str) {
+   mapping alias;
+   string *aliases;
+   string cmd;
+   string arg;
+   int i;
 
-  if( !str || str == "" ) {
-    alias = this_player()->query_aliases();
-    aliases = map_indices( alias );
+   if (!str || str == "") {
+      alias = this_player()->query_aliases();
+      aliases = map_indices(alias);
 
-    if( sizeof( aliases ) == 0 ) {
-      write( "You have no aliases." );
-      return;
-    }
-
-    if (sscanf(str, "-%s",str)) {
-      usage();
-      return;
-    }
-
-    write( "You have defined these aliases: " );
-    for( i = 0; i < sizeof( aliases ); i++ ) {
-      write( "  " + aliases[i] + "='" + alias[aliases[i]] + "'" );
-    }
-  } else if (sscanf(str, "-%s",str)) {
-    usage();
-    return;
-  } else {
-    if( sscanf( str, "%s %s", cmd, arg ) != 2 ) {
-      if( this_player()->is_alias( str ) ) {
-	/* Remove an alias */
-	write( "Alias '" + str + "' (" + this_player()->query_alias( str ) + 
-          ") removed." );
-	this_player()->remove_alias( str );
-	return;
+      if (sizeof(aliases) == 0) {
+	 write("You have no aliases.");
+	 return;
       }
 
+      if (sscanf(str, "-%s", str)) {
+	 usage();
+	 return;
+      }
+
+      write("You have defined these aliases: ");
+      for (i = 0; i < sizeof(aliases); i++) {
+	 write("  " + aliases[i] + "='" + alias[aliases[i]] + "'");
+      }
+   } else if (sscanf(str, "-%s", str)) {
       usage();
       return;
-    }
+   } else {
+      if (sscanf(str, "%s %s", cmd, arg) != 2) {
+	 if (this_player()->is_alias(str)) {
+	    /* Remove an alias */
+	    write("Alias '" + str + "' (" + this_player()->query_alias(str) +
+	       ") removed.");
+	    this_player()->remove_alias(str);
+	    return;
+	 }
 
-    this_player()->add_alias( cmd, arg );
-    write( "Alias '" + cmd + "' (" + arg + ") added." );
-  }
+	 usage();
+	 return;
+      }
+
+      this_player()->add_alias(cmd, arg);
+      write("Alias '" + cmd + "' (" + arg + ") added.");
+   }
 }
--- lib/cmds/player/bug.c
+++ lib/cmds/player/bug.c
@@ -1,86 +1,81 @@
 void usage() {
-  write("Usage: bug [-h] SUBJECT\n");
-  write("Allows you to submit a bug to the system.\n");
-  write("We can't fix bugs unless we know about them.\n");
-  write("Options:\n");
-  write("\t-h\tHelp, this usage message.\n");
+   write("Usage: bug [-h] SUBJECT\n");
+   write("Allows you to submit a bug to the system.\n");
+   write("We can't fix bugs unless we know about them.\n");
+   write("Options:\n");
+   write("\t-h\tHelp, this usage message.\n");
 }
 
 private static mapping msg;
 private static mapping subject;
 private static mapping ob;
 
-void main( string str ) {
+void main(string str) {
    string player_name;
 
-  if ( !str || str == "" ) {
-    usage();
-    return;
-  }
-  if (sscanf(str, "-%s",str)) {
-     usage();
-     return;
-  }
+   if (!str || str == "") {
+      usage();
+      return;
+   }
+   if (sscanf(str, "-%s", str)) {
+      usage();
+      return;
+   }
 
-  if( !msg ) {
-    msg = ([ ]);
-    subject = ([ ]);
-    ob = ([ ]);
-  }
-  player_name = this_player()->query_Name();
+   if (!msg) {
+      msg = ([]);
+      subject = ([]);
+      ob = ([]);
+   }
+   player_name = this_player()->query_Name();
 
-  ob[player_name] = 
-    "/domains/required/rooms/bug_room"->present( "board" );
-  if( !ob[player_name] ) {
-    write( "Unable to locate the bug board. Tell a wizard.\n" );
-    return;
-  } 
+   ob[player_name] = "/domains/required/rooms/bug_room"->present("board");
+   if (!ob[player_name]) {
+      write("Unable to locate the bug board. Tell a wizard.\n");
+      return;
+   }
 
-  if( !str || str == "" ) {
-    subject[player_name] = "[" + 
-      this_player()->query_environment()->file_name() + "]";
-  } else {
-    subject[player_name] = str;
-  } 
+   if (!str || str == "") {
+      subject[player_name] = "[" +
+	 this_player()->query_environment()->file_name() + "]";
+   } else {
+      subject[player_name] = str;
+   }
 
-  write( " \nPlease enter your text below.\n" +
-     "'.' without quotes on a blank line to end. ~a to abort.\n" +
-     "----------------------------------------------------------" +
-     "--------------------\n");
-  this_player()->input_to_object( this_object(), "enter_line" );
-  this_player()->set_editing( 1 );
-  msg[player_name] = "";
+   write(" \nPlease enter your text below.\n" +
+      "'.' without quotes on a blank line to end. ~a to abort.\n" +
+      "----------------------------------------------------------" +
+      "--------------------\n");
+   this_player()->input_to_object(this_object(), "enter_line");
+   this_player()->set_editing(1);
+   msg[player_name] = "";
 }
 
-void enter_line( string line ) {
-  string player_name;
+void enter_line(string line) {
+   string player_name;
 
-  player_name = this_player()->query_Name();
-  if( line != "." && line != "**" && line != "~a" ) {
-    if( !msg[player_name] ) {
-      msg[player_name] = line + "\n";
-    } else {
-      msg[player_name] = msg[player_name] + 
-        line + "\n";
-    }
-    this_player()->input_to_object( this_object(), "enter_line" );
-  } else {
-    if( line == "~a" ) {
-      write( " \nMessage aborted.\n" );
-    } else {
-      ob[player_name]->add_message( player_name,
-        subject[player_name], 
-        msg[player_name] );
-      write( " \nBug report sent! Thank you.\n" );
-      CHANNEL_D->chan_send_string( "wiz", 
-        player_name, 
-        "sends off a bug report.", 1 );
-      
-    }
-    this_player()->set_editing( 0 );
-    this_player()->write_prompt();
-    this_environment()->tell_room( this_player(), player_name + 
-      " lets us all know how much the mud sucks.\n" );
-  }
+   player_name = this_player()->query_Name();
+   if (line != "." && line != "**" && line != "~a") {
+      if (!msg[player_name]) {
+	 msg[player_name] = line + "\n";
+      } else {
+	 msg[player_name] = msg[player_name] + line + "\n";
+      }
+      this_player()->input_to_object(this_object(), "enter_line");
+   } else {
+      if (line == "~a") {
+	 write(" \nMessage aborted.\n");
+      } else {
+	 ob[player_name]->add_message(player_name,
+	    subject[player_name], msg[player_name]);
+	 write(" \nBug report sent! Thank you.\n");
+	 CHANNEL_D->chan_send_string("wiz",
+	    player_name, "sends off a bug report.", 1);
+
+      }
+      this_player()->set_editing(0);
+      this_player()->write_prompt();
+      this_environment()->tell_room(this_player(), player_name +
+	 " lets us all know how much the mud sucks.\n");
+   }
 }
-
