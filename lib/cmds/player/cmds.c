11ad85074331ab650d32b6e5e953273a593e044b
Kent Mein <mein@cs.umn.edu>
2012-03-22 19:41:57 -0500

    Fix indent so it doesn't put spaces between variable declarations.
    Kent

--- lib/cmds/player/cmds.c
+++ lib/cmds/player/cmds.c
@@ -2,112 +2,113 @@ int col;
 string *lines;
 
 void usage() {
-  string flags;
+   string flags;
 
-  flags = "player|verb";
+   flags = "player|verb";
 
-  if (query_wizard(this_player())) flags += "|wiz";
-  if (query_admin(this_player())) flags += "|admin";
+   if (query_wizard(this_player()))
+      flags += "|wiz";
+   if (query_admin(this_player()))
+      flags += "|admin";
 
-  write("Usage: cmds [-h] [-v] [" + flags + "]\n");
-  write("cmds lists the various cmds at your disposal.\n");
-  write("Options:\n");
-  write("\t-h\tHelp, this usage message.\n");
-  write("\t-v\tVertical, displays commands one per line.\n");
-  write("If you specify a subsection like player it will only list " +
-    "player commands, instead of all commands you have available.\n");
-  write("See also: man, help\n");
+   write("Usage: cmds [-h] [-v] [" + flags + "]\n");
+   write("cmds lists the various cmds at your disposal.\n");
+   write("Options:\n");
+   write("\t-h\tHelp, this usage message.\n");
+   write("\t-v\tVertical, displays commands one per line.\n");
+   write("If you specify a subsection like player it will only list " +
+      "player commands, instead of all commands you have available.\n");
+   write("See also: man, help\n");
 }
 
 // XXX not sure on verbs vs cmds might want to merge them...
 // might want to make emote's available as well with a flag... -e
 
-void show_cmds( string dir) {
+void show_cmds(string dir) {
    string *names;
    mapping cmds;
    int c, d, sz;
-   string * files;
+   string *files;
    int i;
- 
-   cmds = ([ ]);
-   files = get_dir( dir + "/*.c" )[0];
-   for( i = sizeof( files ) - 1; i >= 0; i-- ) {
-     cmds[files[i][..strlen(files[i])-3]] = dir;
+
+   cmds = ([]);
+   files = get_dir(dir + "/*.c")[0];
+   for (i = sizeof(files) - 1; i >= 0; i--) {
+      cmds[files[i][..strlen(files[i]) - 3]] = dir;
    }
 
-   names = map_indices( cmds );
+   names = map_indices(cmds);
 
-   lines += ({ "Commands: " + dir });
+   lines += ( { "Commands: " + dir } );
 
-   for( d = 0, sz = sizeof( names ); d < sz; d += col ) {
+   for (d = 0, sz = sizeof(names); d < sz; d += col) {
       int colnum;
       string line;
 
       line = "";
 
-      for( colnum = 0; colnum < col; colnum++ ) {
-         if( d + colnum < sz ) {
-            line += ( names[d + colnum] + 
-               ( col == 1 ? " (" + cmds[names[d+colnum]] + ")" : "" ) +
-               "                                             " +
-               "                         ")[0..(70/col)];
-          }
+      for (colnum = 0; colnum < col; colnum++) {
+	 if (d + colnum < sz) {
+	    line += (names[d + colnum] +
+	       (col == 1 ? " (" + cmds[names[d + colnum]] + ")" : "") +
+	       "                                             " +
+	       "                         ")[0..(70 / col)];
+	 }
       }
-      lines += ({ line });
+      lines += ( { line } );
    }
 
 }
 
-void main( string str ) {
+void main(string str) {
    string *path;
    int c, cmax;
 
-   lines = ({ });
+   lines = ( { } ); 
 
-   if( str == "-v" ) {
+   if (str == "-v") {
       col = 1;
    } else {
       col = 4;
       if (str && str != "") {
-	 switch(str) {
-            case "player":
-                show_cmds("/cmds/player");
-		break;
-            case "verb":
-   	        show_cmds("/cmds/verb");
-		break;
-            case "wiz":
-            case "wizard":
-		if (query_wizard(this_player())) {
-                   show_cmds("/cmds/wiz");
-                } else {
-                   usage();
-                }
-		break;
-            case "admin":
-		if (query_admin(this_player())) {
-                   show_cmds("/kernel/cmds/admin");
-                } else {
-                   usage();
-                }
+	 switch (str) {
+	    case "player":
+	       show_cmds("/cmds/player");
 	       break;
-            default:
-               usage();
-               break;
+	    case "verb":
+	       show_cmds("/cmds/verb");
+	       break;
+	    case "wiz":
+	    case "wizard":
+	       if (query_wizard(this_player())) {
+		  show_cmds("/cmds/wiz");
+	       } else {
+		  usage();
+	       }
+	       break;
+	    case "admin":
+	       if (query_admin(this_player())) {
+		  show_cmds("/kernel/cmds/admin");
+	       } else {
+		  usage();
+	       }
+	       break;
+	    default:
+	       usage();
+	       break;
 	 }
-         this_player()->more(lines);
-         return;
+	 this_player()->more(lines);
+	 return;
       }
    }
 
    path = this_player()->query_path();
 
-   for ( c = sizeof( path ) - 1; c >= 0; c-- ) {
+   for (c = sizeof(path) - 1; c >= 0; c--) {
       show_cmds(path[c]);
-   } 
+   }
 
    show_cmds("/cmds/verb");
 
    this_player()->more(lines);
 }
-
