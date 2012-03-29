bf2facb7c4755d22fd5a8efe3acd6c674293d034
Kent Mein <mein@cs.umn.edu>
2012-03-29 17:17:27 -0500

    Removed the -w flag from diff and made is simple again.
    Kent

--- lib/cmds/wiz/diff.c
+++ lib/cmds/wiz/diff.c
@@ -1,31 +1,21 @@
 void usage() {
-   write("Usage: diff [-h] [-w] [-o FILE] FILE1 FILE2\n");
+   write("Usage: diff [-h] [-o FILE] FILE1 FILE2\n");
+   write("This is the poor man's diff. it uses a mapping to store lines.\n");
+   write("So it only tells you if an exact line is in both files or not.\n");
+   write("It will only let you know about the first occurance of a " +
+      "line as well.\n");
+   write("It works nicely enough for small cases though.  If you need more\n");
+   write("power use a real diff.\n");
    write("Print out the differences between FILE1 and FILE2, " +
       "in std diff format.\n");
    write("Options:\n");
    write("\t-h\tHelp, this usage message.\n");
-   write("\t-w\tIgnore whitespace differences in the line.\n");
    write("\t-o FILE\tPrint the output to FILE instead of to the player.\n");
    write("See also: grep\n");
 }
 
-string rm_spaces(string line) {
-   string str, tmp;
-   int x;
-
-   str = line;
-   x = strstr(str," ");
-   while(x > -1) {
-      str = replace_string(str," ","");
-      x=strstr(str," ");
-   }
-
-   return str;
-}
-
 // Return an array of differences between the files
-// if flag==1 ignore white space in a line
-string *do_work(string file1, string file2, int flag) {
+string *do_work(string file1, string file2) {
    string *lines1, *lines2, *lines, *keys;
    int i, max, c1, c2;
    mapping lns1, lns2;
@@ -45,27 +35,21 @@ string *do_work(string file1, string file2, int flag) {
       return lines;
    }
 
-write("Looking at file: " + file1 + "\n");
    lines1 = explode(read_file(file1), "\n");
    max = sizeof(lines1);
    for(i=0; i< max; i++ ) {
-      if (flag) 
-         tmp = rm_spaces(lines1[i]);
-      else 
-         tmp = lines1[i];
-      lns1[tmp] = i + 1;
+      tmp = lines1[i];
+      if (!lns1[tmp]) 
+         lns1[tmp] = i + 1;
       c1 = 1;
    }
 
-   file2 = normalize_path(file2, this_player()->query_env("cwd"));
    lines2 = explode(read_file(file2), "\n");
    max = sizeof(lines2);
    for(i=0; i< max; i++ ) {
-      if (flag) 
-         tmp = rm_spaces(lines2[i]);
-      else 
-         tmp = lines2[i];
-      lns2[tmp] = i + 1;
+      tmp = lines2[i];
+      if (!lns2[tmp]) 
+         lns2[tmp] = i + 1;
       c2 = 1;
    }
 
@@ -78,7 +62,6 @@ write("Looking at file: " + file1 + "\n");
    } else if (lns1 == lns2) {
       write("Files match.\n");
    } else {
-
       keys = map_indices(lns1);
       max = sizeof(keys) - 1;
 
@@ -86,7 +69,7 @@ write("Looking at file: " + file1 + "\n");
       for(i=0; i<max; i++) {
          tmp = keys[max-i];
          if(tmp && !lns2[tmp]) {
-            lines += ({ lns1[tmp] + " : " + lines1[lns1[tmp]] });
+            lines += ({ lns1[tmp] + " : " + tmp });
          }
       }
 
@@ -101,7 +84,7 @@ write("Looking at file: " + file1 + "\n");
       for(i=0; i<max; i++) {
          tmp = keys[max-i];
          if(tmp && !lns1[tmp]) {
-            lines += ({ lns2[tmp] + " : " + lines2[lns2[tmp]] });
+            lines += ({ lns2[tmp] + " : " + tmp });
          }
       }
    }
@@ -109,7 +92,7 @@ write("Looking at file: " + file1 + "\n");
    return lines;
 }
 
-void do_work_tofile(string outfile, string file1, string file2, int flag) {
+void do_work_tofile(string outfile, string file1, string file2) {
    string *lines;
    int i, max;
 
@@ -117,7 +100,7 @@ void do_work_tofile(string outfile, string file1, string file2, int flag) {
    if (!valid_write(outfile)) {
       write("Unable to write to file: " + outfile + "\n");
    } else {
-      lines = do_work(file1, file2, flag);
+      lines = do_work(file1, file2);
       max = sizeof(lines);
       for (i=0;i<max;i++) {
          write_file(outfile,lines[i]);
@@ -125,36 +108,26 @@ void do_work_tofile(string outfile, string file1, string file2, int flag) {
    }
 }
 
-void do_work_toscreen(string file1, string file2, int flag) {
+void do_work_toscreen(string file1, string file2) {
    string *lines;
  
-   lines = do_work(file1, file2, flag);
+   lines = do_work(file1, file2);
    this_player()->more(lines);
 }
 
 void main(string str) {
    string outfile, file1, file2;
-   int flag;
 
    if (!str || str == "" || sscanf(str, "-h", str)) {
       usage();
       return;
    }
 
-   flag = 0;
-   if (sscanf(str,"-w %s",str)) {
-      flag = 1;
-   } else if (strstr(str," -w") > -1) {
-      str = replace_string(str," -w","");
-      flag = 1;
-   }
-
-
    if (sscanf(str, "-o %s %s %s", outfile, file1, file2)) {
-      do_work_tofile(outfile, file1, file2, flag);
+      do_work_tofile(outfile, file1, file2);
    } else {
       if (sscanf(str, "%s %s", file1, file2)) {
-	 do_work_toscreen(file1, file2, flag);
+	 do_work_toscreen(file1, file2);
       } else {
          usage();
 	 return;
