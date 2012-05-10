13522660f10385ea497fd85415002dab302eb604
Kent Mein <mein@cs.umn.edu>
2012-05-10 03:06:03 -0500

    Added a play function to piano and tried to get it so you could read the bandana... Both still need work...
    Kent

--- lib/domains/boothill/obj/bandana.c
+++ lib/domains/boothill/obj/bandana.c
@@ -1,4 +1,5 @@
 inherit ARMOR;
+inherit M_READABLE;
 
 string get_color() {
 
@@ -27,8 +28,7 @@ int set_color(string str) {
 }
 
 void setup(void) {
-
-   set_id("bandana", "scarf");
+   set_id("bandana", "scarf", "label");
    set_color("");
 
    set_gettable(1);
@@ -36,24 +36,18 @@ void setup(void) {
    set_wear_message("$N $vput $o on.");
    set_remove_message("$N $vtake off $o.");
 
-   add_object_command("shake","do_shake");
-// XXX Need to add call to check_label....
+   add_object_command("shake","do_shakeit");
+
+// XXX why doesn't this work?
+   set_message("The label on the bandana reads: Hand wash only.\n" +
+      "Shake vigirisly to dry.");
 }
 
-int check_label(string str) {
-   write("You take a peek at the label and it states: Hand wash only.  " +
-      "Shake vigiorisly to dry.");
-   this_player()->query_environment()->tell_room(this_player(),
-      this_player()->query_Name() + " takes a peek at a label on their " +
-      "bandana.\n");
-   return 1;
-}
-
-int do_shake(string str) {
+int do_shakeit(string str) {
    if (str == "scarf" || str == "bandana") {
-      write("You vigiorisly shake the bandana.");
+      write("You vigorously shake the bandana.");
       this_player()->query_environment()->tell_room(this_player(),
-         this_player()->query_Name() + "vigiorisly shakes a colorful " +
+         this_player()->query_Name() + " vigorously shakes a colorful " +
          "bandana.\n");
       write("The bandana shifts it's color.");
 
--- lib/domains/boothill/obj/piano.c
+++ lib/domains/boothill/obj/piano.c
@@ -5,4 +5,39 @@ void setup(void) {
    set_long("A pretty beat up bar piano.  This thing looks like " +
       "it has seen better days.  It seems to beckon you to play it.");
    set_in_room_desc("A rough looking piano.");
+
+   // XXX why doesn't this work?
+   add_object_command("play","do_play");
+}
+
+int do_play(string str) {
+   int x;
+
+   if (str == "piano") {
+      x = random(5);
+      switch(x) {
+         case 0:
+            write("You play a little ditty.");
+            this_environment()->tell_room(this_player(), 
+               this_player()->query_Name() + " plays a little ditty.");
+            break;
+         case 1:
+            write("You stroke the keys with love.");
+            this_environment()->tell_room(this_player(), 
+               this_player()->query_Name() + " plays a sweet little number.");
+            break;
+         case 2:
+            write("You bring it home.");
+            this_environment()->tell_room(this_player(), 
+               this_player()->query_Name() + " stuns you with their " + 
+               "rendition of chopin.");
+            break;
+         default:
+            write("You work the keyboard like it was your best friend.");
+            this_environment()->tell_room(this_player(), 
+               this_player()->query_Name() + " plays an old country song.");
+            break;
+      }
+   }
+   return 0;
 }
