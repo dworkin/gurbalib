39eadb409a6591851b359abdca221fcd04ea72c8
Kent Mein <mein@cs.umn.edu>
2012-06-10 09:07:53 -0500

    Small bit of work on boothill rooms.
    Kent

--- lib/domains/boothill/rooms/boothill.c
+++ lib/domains/boothill/rooms/boothill.c
@@ -6,11 +6,35 @@ void setup(void) {
    set_brief("Boothill");
    set_long("If your not sure what boothill is, now you know, its a " +
       "cemetary just outside of town.  There are a bunch of cheap " +
-      "graves here.  Including a freshly dug hole, just waiting for the " +
-      "towns next victum.");
+      "graves here.  With Odd looking tombstones.  There is also  a " +
+      "freshly dug hole, just waiting for the towns next victum.");
 
    set_exits(([
       "south" : "/domains/boothill/rooms/infront_jail.c", 
+      "down" : "/domains/boothill/roomes/hole.c",
    ]));
 
+}
+
+void look_at_stone() {
+   int x;
+
+   x = random(5);
+   switch (x) {
+      case 0:
+         write("Here lies Fred, with a belly full of lead!"\n");
+         break;
+      case 1:
+         write("Here lies Jake, He got hit over the head with a rake."\n");
+         break;
+      case 2:
+         write("Here lies Old man Tucker, He couldn't hold his liquor."\n");
+         break;
+      case 3:
+         write("We don't know this poor feller.\n");
+         break;
+      case 4:
+         write("Here lies the fine Miss Sally.\n");
+         break;
+   }
 }
---
+++ lib/domains/boothill/rooms/hole.c
@@ -0,0 +1,12 @@
+inherit ROOM;
+
+void setup(void) {
+   add_area("boothill");
+
+   set_brief("A crudely dug hole");
+   set_long("Your average hole in the ground.");
+
+   set_exits(([
+      "up" : "/domains/boothill/rooms/boothill.c", 
+   ]));
+}
