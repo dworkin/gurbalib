3fb73e2b6e7241e501c202188838f26f12f6ae18
Kent Mein <mein@cs.umn.edu>
2012-06-15 17:12:12 -0500

    Added a missing object.  It still needs work.
    Kent

---
+++ lib/domains/2.4.5/obj/torch.c
@@ -0,0 +1,13 @@
+inherit OBJECT;
+
+void setup(void) {
+   set_id("torch");
+   set_adj("large");
+   set_long("A stick with some burnable stuff stuck to the end of it.");
+   set_in_room_desc("A torch");
+   set_gettable(1);
+
+   set_value(1);
+   // XXX set_weight(10);
+   // Also need to make it lightable????
+}
