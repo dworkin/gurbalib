3f031a5d69248923e9909eaad555029037e7d1e5
Kent Mein <mein@cs.umn.edu>
2012-06-18 11:40:52 -0500

    Fix up number of monsters in the given room...
    Kent

--- lib/domains/2.4.5/rooms/orc_treasure.c
+++ lib/domains/2.4.5/rooms/orc_treasure.c
@@ -9,9 +9,7 @@ void setup(void) {
    add_exit("south","/domains/2.4.5/rooms/fortress.c");
 
    set_objects (([
-      "/domains/2.4.5/monsters/orc_slayer.c": 1,
       "/domains/2.4.5/monsters/orc_shayman.c": 1,
    ]));
 }
 
-// XXX add staff and sword on the monsters???
