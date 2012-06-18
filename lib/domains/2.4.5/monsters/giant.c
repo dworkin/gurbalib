925f4add11c884e666106d9e6c05b0a138fec914
Kent Mein <mein@cs.umn.edu>
2012-06-18 12:05:15 -0500

    Yet more work...
    Kent

--- lib/domains/2.4.5/monsters/giant.c
+++ lib/domains/2.4.5/monsters/giant.c
@@ -4,21 +4,15 @@ inherit M_TRIGGERS;
 inherit M_BLOCKEXITS;
 
 void setup() {
-   set_name("troll");
-   /* Leave out the gender specification, get a random one */
-   /*  set_gender( "male" ); */
-   add_adj("smelly");
-   set_in_room_desc("A large smelly troll.");
-   set_long("The troll looks like a messed up tree.");
-   set_race("troll");
-   set_max_hp(100);
+   set_name("giant");
+   // XXX set_level(15):
+   set_in_room_desc("A giant");
+   // XXX set_aggressive(1);
+   set_long("A massive giant.");
+   set_race("giant");
+   set_max_hp(300);
    set_hit_skill("combat/unarmed");
    set_skill("combat/unarmed", 50);
    set_skill("combat/defense", 100);
 }
-
-// XXX Make the troll talk...
-//  The troll says: Mumble...
-//  The troll says: Your mother was a %(#GKB!
-// See room code forest2.c in 2.4.5 orlginally for specifics...
 
