ed59a09de0722b11dc71e9d0af03f00aa5de2ef2
Kent Mein <mein@cs.umn.edu>
2012-06-26 09:54:08 -0500

    Added some missing objects...
    Kent

---
+++ lib/domains/2.4.5/obj/axe.c
@@ -0,0 +1,22 @@
+inherit WEAPON;
+
+void setup(void) {
+   set_id("axe");
+   set_long("A wicked axe with a well worn wooden handle.");
+   set_in_room_desc("An axe.");
+   set_gettable(1);
+
+   set_min_damage(1);
+   set_max_damage(4);
+   set_hit_bonus(0);
+
+   set_weapon_skill("combat/sharp/small");
+
+   set_wield_type("single");
+   set_wield_message("$N $vwield $p $o.");
+   set_unwield_message("$N $vloosen $p $vgrip on $o, and $vput it away.");
+   set_weapon_action("slash");
+   set_value(10);
+   set_size(10);
+   set_weight(2);
+}
---
+++ lib/domains/2.4.5/obj/curved_knife.c
@@ -0,0 +1,22 @@
+inherit WEAPON;
+
+void setup(void) {
+   set_id("knife");
+   set_long("A small sharp knife with a curved blade.");
+   set_in_room_desc("A curved knife.");
+   set_gettable(1);
+
+   set_min_damage(1);
+   set_max_damage(3);
+   set_hit_bonus(0);
+
+   set_weapon_skill("combat/sharp/small");
+
+   set_wield_type("single");
+   set_wield_message("$N $vwield $p $o.");
+   set_unwield_message("$N $vloosen $p $vgrip on $o, and $vput it away.");
+   set_weapon_action("slash");
+   set_value(8);
+   set_size(10);
+   set_weight(2);
+}
