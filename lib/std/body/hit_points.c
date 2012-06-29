bc2d2fd0f63cf8f6259fd256283576247c952d0e
Kent Mein <mein@cs.umn.edu>
2012-06-28 22:39:07 -0500

    Make sure the presidence in the code is clear...
    Kent

--- lib/std/body/hit_points.c
+++ lib/std/body/hit_points.c
@@ -117,7 +117,7 @@ string query_status(void) {
    if (x == max)
       return "in peak physical condition.";
    if (max < 5) {
-      if (x < max / 2)
+      if (x < (max / 2))
 	 return "nocking on deaths door.";
       return "wounded.";
    } else {
