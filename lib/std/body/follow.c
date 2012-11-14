6c99f6fab3934ca9056f74b41e0415681d42c87f
Kent Mein <mein@cs.umn.edu>
2012-11-13 22:19:27 -0600

    Forgot to add this file earlier...
    Kent

---
+++ lib/std/body/follow.c
@@ -0,0 +1,14 @@
+object *followers;
+
+int add_follower(object who) {
+   return 1;
+}
+
+int remove_follower(object who) {
+   return 1;
+}
+
+/* return 1 if who is a follower */
+int query_follower(object who) {
+   return 0;
+}
