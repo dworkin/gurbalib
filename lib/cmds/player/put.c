5fb063c7a30d7583f2deeb51abd1a3ab91b9db4f
Kent Mein <mein@cs.umn.edu>
2012-11-26 17:33:39 -0600

    Fix the put command....
    Kent

--- lib/cmds/player/put.c
+++ lib/cmds/player/put.c
@@ -11,8 +11,8 @@ void do_put(object obj1, object obj2, int loud) {
    string slot;
    object worn;
 
-   if (loud && !obj) {
-      write("what are you trying to put where?");
+   if (loud && !obj1) {
+      write("What are you trying to put where?");
       return;
    }
 
@@ -26,43 +26,44 @@ void do_put(object obj1, object obj2, int loud) {
       return;
    }
 
-   if (obj->is_worn()) {
-      if (obj->is_cursed()) {
+   if (obj1->is_worn()) {
+      if (obj1->is_cursed()) {
          this_player()->targetted_action("$N $vtry to remove $o, but $vfumble.",
-            nil, obj);
+            nil, obj1);
          write("Strange... It won't come off.\n");
          return;
       } else {
-         this_player()->do_remove(obj);
-         this_player()->targetted_action(obj->query_remove_message(), nil, obj);
+         this_player()->do_remove(obj1);
+         this_player()->targetted_action(obj1->query_remove_message(), nil, obj1);
       }
    }
 
-   if (obj->is_wielded()) {
-      if (obj->is_cursed()) {
+   if (obj1->is_wielded()) {
+      if (obj1->is_cursed()) {
          this_player()->targetted_action("$N $vtry to unwield $o, " +
-            "but $vfumble.", nil, obj);
+            "but $vfumble.", nil, obj1);
          write("Strange... You can't unwield that..\n");
          return;
       } else {
-         this_player()->do_unwield(obj);
-         this_player()->targetted_action(obj->query_unwield_message(), 
-            nil, obj);
+         this_player()->do_unwield(obj1);
+         this_player()->targetted_action(obj1->query_unwield_message(), 
+            nil, obj1);
       }
    }
 
-   if (obj->move(obj2)) {
-      this_player()->targetted_action("$N $vput $o in $o1.", nil, obj, obj2);
+   if (obj1->move(obj2)) {
+      this_player()->targetted_action("$N $vput $o in $o1.", nil, obj1, obj2);
    } else {
       this_player()->targetted_action("$N $vtryto  put $o in $o1, but $vfail.",
-         nil, obj, obj2);
+         nil, obj1, obj2);
    }
 }
 
 void main(string str) {
-   object obj;
+   object obj, obj2;
    object *inv;
    int i, max;
+   string what, where;
 
    if (!str || str == "") {
       usage();
@@ -74,15 +75,35 @@ void main(string str) {
       return;
    }
 
-   if (str == "all") {
+   if (sscanf(str, "%s in %s",what,where) == 2) {
+   } else if (sscanf(str, "%s at %s",what,where) == 2) {
+   } else if (sscanf(str, "%s %s",what,where) == 2) {
+   } else {
+      write("You want to put what where?");
+      return;
+   }
+
+   obj = this_player()->present(lowercase(where));
+   if (!obj) {
+      obj = this_player()->query_environment()->present(lowercase(where));
+   }
+   if (!obj) {
+      write("Where are you trying to put that?");
+      return;
+   }
+
+   if (what == "all") {
       inv = this_player()->query_inventory();
       max = sizeof(inv);
       for (i = 0; i < max; i++) {
-         do_put(inv[i],0);
+         do_put(inv[i], obj, 0);
       }
       return;
    }
 
-   obj = this_player()->present(lowercase(str));
-   do_put(obj,1);
+   obj2 = this_player()->present(lowercase(what));
+   if (!obj2) {
+      obj2 = this_player()->query_environment()->present(lowercase(what));
+   }
+   do_put(obj2, obj, 1);
 }
