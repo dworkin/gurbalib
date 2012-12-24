fe181b6f3673e2b12894f5178d206252b8b6bf17
Kent Mein <mein@cs.umn.edu>
2012-12-24 03:51:29 -0600

    More work on lockers...
    Kent

--- lib/domains/gurba/guilds/fighter/rooms/storage.c
+++ lib/domains/gurba/guilds/fighter/rooms/storage.c
@@ -8,24 +8,28 @@ void setup(void) {
 
    set_exits(([
       "south" : DIR + "/guilds/fighter/rooms/main",
+      "east" : "#go_locker",
    ]));
 }
 
-void go_locker {
+void go_locker(void) {
    string locker;
    object obj;
 
-   locker = REQDIR + "data/lockers/" + this_player()->query_name() + ".o";
+   locker = DIR + "data/lockers/" + this_player()->query_name() + ".o";
 
    if (file_exists(locker)) {
       obj = find_object(locker);
       if (!obj) {
       }
    } else {
-      obj = clone_object(REQDIR + "obj/locker.c");
+      obj = clone_object(DIR + "obj/locker.c");
       obj->set_player_name(this_player()->query_name());
       obj->save_me();
    }
 
-   /* XXX Do move to obj.... here */
+   this_object()->event("body_leave", this_player());
+   tell_room(this_player(), this_player()->query_Name() + " leaves east.\n");
+   obj->event("body_enter", this_player());
+   obj->tell_room(this_player(), this_player()->query_Name() + " enters.\n");
 }
