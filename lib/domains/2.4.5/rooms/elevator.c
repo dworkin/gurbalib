de891757d5062749b0a93d4d4607295e9645475f
Kent Mein <mein@cs.umn.edu>
2012-12-28 15:10:08 -0600

    Some extra work on the elevator.
    Kent

--- lib/domains/2.4.5/rooms/elevator.c
+++ lib/domains/2.4.5/rooms/elevator.c
@@ -1,14 +1,37 @@
 #include "../domain.h"
 inherit ROOM;
+int location, destination;
+
+void set_floor(int x) {
+   destination = x;
+   switch (location) {
+      case 1:
+         break;
+      case 2:
+         break;
+      case 3:
+         break;
+   }
+}
 
 void setup(void) {
    add_area("2.4.5");
 
    set_brief("Elevator");
-   set_long("You are in the elevator.  On the wall are three buttons, " +
-      "numbered 1 to 3.");
 
+   location = 1;
+   set_floor(1);
    add_exit("north", DIR + "/rooms/vill_shore.c");
+}
+
+string query_long() {
+   string str;
+
+   str = "You are in the elevator.  On the wall are three buttons, " +
+      "numbered 1 to 3.\nCurrently the number " + destination + 
+      " button is lit.";
+
+   return str;
 }
 
 /* XXX Need to do the door, button and stuff.... */
