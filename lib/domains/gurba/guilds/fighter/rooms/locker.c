2580a89d67b053b86c8c3ac15a91ee488f417f56
Kent Mein <mein@cs.umn.edu>
2013-01-27 22:20:01 -0600

    Yet more locker work.
    Kent

--- lib/domains/gurba/guilds/fighter/rooms/locker.c
+++ lib/domains/gurba/guilds/fighter/rooms/locker.c
@@ -24,6 +24,29 @@ void restore_me(void) {
    set_brief(capitalize(player_name) + "'s locker room");
 }
 
+void clean_up(void) {
+   int x, y, max;
+   object *objs;
+   myitems = ({});
+
+   objs = this_object()->query_inventory();
+   if (objs) {
+      max = sizeof(objs);
+   } else {
+      max = 0;
+   }
+   max = max - 1;
+
+   for (x=max; x>=0; x--) {
+      if (objs[x]->is_living()) {
+         objs[x]->move(VOID);
+      } else {
+         objs[x]->destruct();
+      }
+      destruct();
+   }
+}
+
 void save_me(void) {
    int x, y, max;
    object *objs;
@@ -41,7 +64,7 @@ void save_me(void) {
    for (x=0; x<max; x++) {
       if (objs[x]->is_living()) {
       } else {
-         myitems[y] = objs[x]->base_name();
+         myitems += ({ objs[x]->base_name() });
          unguarded("save_object", DIR + "/data/lockers/" + player_name + 
             "/" + y + ".o");
          y = y + 1;
@@ -60,9 +83,40 @@ void setup(void) {
    set_brief(" An unknown locker room");
    set_long("You can store your stuff here.");
 
+   add_room_command("save","do_save");
+
    set_exits(([
       "west" : DIR + "/guilds/fighter/rooms/storage.c",
    ]));
 }
 
 /* On exit need to save_me and destroy this object... XXX */
+int do_save(string str) {
+   object obj;
+   string file;
+
+   file = DIR + "/guilds/fighter/rooms/storage.c";
+
+   if (!(obj = find_object(file))) {
+      catch {
+         obj = compile_object(file);
+         obj->setup();
+         obj->setup_mudlib();
+      } : {
+         write("Could not load " + str);
+      }
+   }
+
+   if (this_player()->move(obj)) {
+      /* XXX Need to move this stuff to other move's like summon/goto */
+      this_object()->event("body_leave", this_player());
+      tell_room(this_player(), this_player()->query_Name() + " leaves east.\n");
+      obj->tell_room(this_player(), this_player()->query_Name() + " enters.\n");
+      this_player()->do_look(0);
+   } else {
+      write("Error going there...\n");
+   }
+
+   save_me();
+   clean_up();
+}
