bd1694b17a54fbb80a0d7c78c909f620a9fb3e89
Kent Mein <mein@cs.umn.edu>
2013-04-12 00:44:27 -0500

    Removing unused stuff.

--- lib/std/modules/m_gettable_fake_object.c
+++
@@ -1,9 +0,0 @@
-static string gettable_fake_object;
-
-string is_gettable_fake_object(void) {
-   return gettable_fake_object;
-}
-
-void set_gettable_fake_object(string get) {
-   gettable_fake_object = get;
-}
