768a167f88ca1f7bc7d9e2e71f171bcd3153f219
Kent Mein <mein@cs.umn.edu>
2013-05-09 23:27:14 -0500

    Work on the lever.  Still not done but its a start.

--- lib/domains/2.4.5/rooms/sub/door_trap.c
+++ lib/domains/2.4.5/rooms/sub/door_trap.c
@@ -1,6 +1,9 @@
 #include "../../domain.h"
 
-inherit "std/room";
+inherit "/std/room";
+inherit "/std/modules/m_block_exits";
+
+int trap;
 
 void setup(void) {
    add_area("2.4.5");
@@ -12,4 +15,31 @@ void setup(void) {
    /* Need to do door stuff here XXX */
    add_exit("east", DIR + "/rooms/well.c");
    add_exit("west", DIR + "/rooms/sub/after_trap.c");
+
+   add_block("east");
+}
+
+int do_block(object who) {
+   if (!trap) {
+      write("The door is blocked.\n");
+      return 1;
+   } else {
+      return 0;
+   }
+}
+
+int toggle_door(string str) {
+   write("You pull the lever.\n");
+   this_player()->query_environment()->tell_room(this_player(),
+      this_player()->query_Name() + " pulls the lever.\n");
+   if (trap) {
+      trap = 0;
+   } else {
+      trap = 1;
+   }
+   return 1;
+}
+
+int query_trap(string str) {
+   return trap;
 }
--- lib/domains/2.4.5/rooms/well.c
+++ lib/domains/2.4.5/rooms/well.c
@@ -1,6 +1,7 @@
 #include "../domain.h"
 
 inherit "/std/room";
+inherit "/std/modules/m_block_exits";
 
 void setup(void) {
    add_area("2.4.5");
@@ -10,8 +11,32 @@ void setup(void) {
       "There is a lever beside a door to the west.");
 
    add_exit("north", DIR + "/rooms/maze/maze1.c");
+   add_exit("west", DIR + "/rooms/sub/door_trap.c");
    add_exit("up", DIR + "/rooms/narr_alley.c");
 
+   add_item("lever", "A short wooden handle, prehaps you can pull it.");
+
+   add_room_command("pull", "pull_lever");
+   add_room_command("turn", "pull_lever");
+
+   add_block("west");
 }
 
 /* XXX Need to add the lever and do what it does... */
+
+int pull_lever(string str) {
+   if (!str || str != "lever") return 0;
+   if (call_other(this_player(),DIR + "/rooms/sub/door_trap", "toggle_door")) {
+      return 1;
+   }
+   return 0;
+}
+
+int do_block(object who) {
+   if (!call_other(this_player(),DIR + "/rooms/sub/query_trap", "query_trap")) {
+      write("The door is blocked.\n");
+      return 1;
+   } else {
+      return 0;
+   }
+}
