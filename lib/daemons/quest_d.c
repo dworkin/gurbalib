6c6e04d9f9bc052501b315bea6071e2bc5ab830b
Kent Mein <mein@cs.umn.edu>
2013-05-15 10:16:33 -0500

    More work on the quest daemon.

--- lib/daemons/quest_d.c
+++ lib/daemons/quest_d.c
@@ -1,4 +1,5 @@
-mapping quests;
+
+mapping quests, levels;
 string *keys;
 
 #define DATAFILE "/daemons/data/quest.o"
@@ -6,48 +7,55 @@ string *keys;
 int add_quest(string name, int level, string contacts) {
    int i, max, index;
 
-   if (exists(quests{name})) {
+   if (quests[name]) {
       write("I'm sorry but there is already a quest titled: " + name + "\n");
       return 0;
    }
 
    max = sizeof(keys);
    index = 0;
-   for(i=0;i<max;i++) {
-      if (level > quests{keys[i]->level}) {
+   for(i=0; i<max; i++) {
+      if (level > levels[keys[i]]) {
          index = index + 1;
       }
    }
    /* Put name in keys at index */
 
    /* Add mapping values */
-   quests{name}->level = level;
-   quests{name}->contacts = contacts;
-
+   levels[name] = level;
+   quests[name] = contacts;
 }
 
+/* XXX Not done */
 int remove_quest(string name) {
 }
 
 void list_quests() {
-}
+   int i, max;
 
-void create(void) {
-   if (file_exists(DATAFILE) {
-      restore_me();
-   } else {
-   quests = ([]);
-   keys = ();
-   add_quest("NewbieVille",1,"sirdude");
-   save_me();
+   max = sizeof(keys);
+   for(i=0; i<max; i++) {
+      write(levels[keys[i]] + "\t" + keys[i] + "\t" + quests[keys[i]] + "\n");
+   }
 }
 
 static void restore_me(void) {
    unguarded("restore_object", DATAFILE);
-
 }
 
 static void save_me(void) {
    unguarded("save_object", DATAFILE);
+}
+
+void create(void) {
+   if (file_exists(DATAFILE)) {
+      restore_me();
+   } else {
+      quests = ([]);
+      levels = ([]);
+      keys = ({});
+      add_quest("NewbieVille",1,"sirdude");
+      save_me();
+   }
 }
 
