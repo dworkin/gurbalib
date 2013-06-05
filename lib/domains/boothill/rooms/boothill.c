8a358a1f2404173b215b1c5da9c5c5fc437d9f64
Kent Mein <mein@cs.umn.edu>
2013-06-05 11:14:34 -0500

    Fix a small typo and add a description of the hole.

--- lib/domains/boothill/rooms/boothill.c
+++ lib/domains/boothill/rooms/boothill.c
@@ -8,7 +8,7 @@ void setup(void) {
    set_short("Boothill");
    set_long("If your not sure what boothill is, now you know, its a " +
       "cemetary just outside of town.  There are a bunch of cheap " +
-      "graves here.  With Odd looking tombstones.  There is also  a " +
+      "graves here.  With Odd looking tombstones.  There is also a " +
       "freshly dug hole, just waiting for the towns next victum.");
 
    set_exits(([
@@ -19,5 +19,9 @@ void setup(void) {
    set_objects(([
       DIR + "/obj/tombstone.c" : 1,
    ]));
+
+  add_item("hole", "It's actually a fairly deep hole.  " +
+     "It's hard to see into it from up here.");
+
 }
 
