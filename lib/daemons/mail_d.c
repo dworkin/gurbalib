4ef26599be2e242215ee415222fb52a5a597c020
Kent Mein <mein@cs.umn.edu>
2013-07-15 17:50:28 -0500

    More work on the mail daemon.

--- lib/daemons/mail_d.c
+++ lib/daemons/mail_d.c
@@ -2,6 +2,83 @@
 #define EMAIL_INDEX "/domains/required/objects/email_index.c"
 #define DATA_DIR "/daemons/data/email"
 
+/* XXX This is a work in progress, need to convert index to a mapping 
+   and flesh it out */
+
+string get_filename(string who) {
+   string filen;
+
+   filen = DATA_DIR + "/" + who + "/" + time();
+   while (file_exists(filen)) {
+     filen += "1";
+   }
+   return (filen);
+}
+
+string *load_index(string who) {
+   object myindex;
+
+   myindex = clone_object(EMAIL_INDEX);
+   myindex->restore_me(DATA_DIR + "/" + who + "/index.o");
+}
+
+string *save_index(string who, string *index) {
+   object myindex;
+
+   myindex->save_me(DATA_DIR + "/" + who + "/index.o");
+}
+
+int add_index(string who, object mail, string filename) {
+   string *index;
+   string line;
+
+   index = load_index(who);
+
+   line = mail->query_from() + ": " + mail->query_subject();
+   index += ({ line });
+
+   save_index(who,index);
+}
+
+int delete_index(string who, int num) {
+   string *index;
+   int x, max;
+
+   index = load_index(who);
+
+   if (num > index) {
+      return 0;
+   }
+
+   max = sizeof(index);
+   while (num < max) {
+      index[num] = index[num +1];
+   }
+   index[max] = nil;
+
+   save_index(who,index);
+}
+
+int write_mail(string who, object mail) {
+   string filename;
+   object usr;
+
+   filename = get_filename(who);
+   if (mail->save_me(DATA_DIR + "/" + who + "/" + filename)) {
+      if (add_index(who,mail,filename)) {
+
+         if (usr = USER_D->find_player(who)) {
+            usr->message("You have new email.\n");
+         }
+         return 1;
+      } else {
+         return 0;
+      }
+   } else {
+      return 0;
+   }
+}
+
 object get_mail(string who, string file) {
    string filename;
    object email_obj;
@@ -29,43 +106,3 @@ int check_new_email(object who) {
 
    name = who->query_name();
 }
-
-string get_filename(string who) {
-   string filen;
-
-   filen = DATA_DIR + "/" + who + "/" + time();
-   while (file_exists(filen)) {
-     filen += "1";
-   }
-   return (filen);
-}
-
-int add_index(string who, object mail, string filename) {
-}
-
-int delete_index(string who, int index) {
-}
-
-string *get_index(string who) {
-}
-
-int write_mail(string who, object mail) {
-   string filename;
-   object usr;
-
-   filename = get_filename(who);
-   if (mail->save_me(DATA_DIR + "/" + who + "/" + filename)) {
-      if (add_index(who,mail,filename)) {
-
-         if (usr = USER_D->find_player(who)) {
-            usr->message("You have new email.\n");
-         }
-         return 1;
-      } else {
-         return 0;
-      }
-   } else {
-      return 0;
-   }
-}
-
