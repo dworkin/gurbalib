6e6f5d0197576838b6e48c131dc10a2009bef815
Kent Mein <mein@cs.umn.edu>
2013-07-22 13:00:10 -0500

    Fix output of the where command so its a little nicer.

--- lib/cmds/wiz/where.c
+++ lib/cmds/wiz/where.c
@@ -31,16 +31,16 @@ void main(string str) {
       usrs = USER_D->query_players();
 
       for (i = 0; i < sizeof(usrs); i++) {
-	 write(usrs[i]->query_Name() + " " + 
-            usrs[i]->query_environment()->query_short() + " " +
+	 write(usrs[i]->query_Name() + " Loc: (" + 
+            usrs[i]->query_environment()->query_short() + ") " +
 	    usrs[i]->query_environment()->base_name() + "\n");
       }
    } else {
       if (sscanf(str, "-%s", str)) {
 	 usage();
       } else if (usr = USER_D->find_player(str)) {
-	 write(usr->query_Name() + " " +
-	    usr->query_environment()->query_short() + " " +
+	 write(usr->query_Name() + " Loc: (" +
+	    usr->query_environment()->query_short() + ") " +
 	    usr->query_environment()->base_name() + "\n");
       } else {
 	 write("Who are you looking for?  I can't find: " + str + "!\n");
