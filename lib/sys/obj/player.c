1ad6a680f78b48efaafa1cab7e9510657a3246fb
Kent Mein <mein@cs.umn.edu>
2013-07-26 15:27:55 -0500

    Fix up the formatting of the file a little.

--- lib/sys/obj/player.c
+++ lib/sys/obj/player.c
@@ -116,13 +116,10 @@ void login_player(void) {
       EVENT_D->event("player_login", living_name);
    }
 
-   /* Gender less? Set the player to male */
-   if (query_gender() == "unkown")
-      set_gender("male");
-
    /* Set the current working directory */
-   if (!query_env("cwd"))
+   if (!query_env("cwd")) {
       set_env("cwd", "/");
+   }
 
    /* Subscribe to default channels */
    if (!channels) {
@@ -143,9 +140,9 @@ void login_player(void) {
    set_hit_skill("combat/unarmed");
    ANSI_D->set_player_translations(custom_colors);
    autoload = query_env("autoload");
-   if (autoload == nil)
+   if (autoload == nil) {
       autoload = 0;
-   if (autoload == 1) {
+   } else if (autoload == 1) {
       this_player()->clone_autoload_objects();
    }
 }
@@ -192,8 +189,9 @@ int query_ansi(void) {
    mixed x;
 
    x = query_env("ansi");
-   if (typeof(x) == T_NIL)
+   if (typeof(x) == T_NIL) {
       return 1;		/* ansi is on if query_env("ansi") is undefined */
+   }
    return x;
 }
 
@@ -219,8 +217,9 @@ string query_title(void) {
       return "";
    }
 
-   if (!t || t == "")
+   if (!t || t == "") {
       t = "$N the title less";
+   }
    t2 = replace_string(t, "$N", capitalize(living_name));
    if (t2 == t)  t2 = capitalize(living_name) + " " + t;
 
@@ -244,10 +243,11 @@ void set_linkdead(int flag) {
 }
 
 void set_editing(int flag) {
-   if (flag == 1)
+   if (flag == 1) {
       set_short(query_title() + " [editing]");
-   else
+   } else {
       set_short(query_title());
+   }
 }
 
 int query_linkdead(void) {
@@ -297,15 +297,17 @@ string *query_path(void) {
 }
 
 void add_channel(string chan) {
-   if (!channels)
+   if (!channels) {
       channels = ( { } );
+   }
    channels -= ( { chan } );
    channels += ( { chan } );
 }
 
 void remove_channel(string chan) {
-   if (!channels)
+   if (!channels) {
       channels = ( { } );
+   }
    channels -= ( { chan } );
 }
 
@@ -314,27 +316,31 @@ string *query_channels(void) {
 }
 
 void add_ignore(string who) {
-   if (who == "")
+   if (who == "") {
       return;
+   }
    ignored += ( { lowercase(who) } );
 }
 
 void remove_ignore(string who) {
-   if (!ignored)
+   if (!ignored) {
       ignored = ( { } );
+   }
    ignored -= ( { lowercase(who) } );
 }
 
 int query_ignored(string who) {
-   if (!ignored)
+   if (!ignored) {
       ignored = ( { } );
+   }
    who = lowercase(who);
    return member_array(who, ignored) >= 0;
 }
 
 string *query_ignored_all() {
-   if (!ignored)
+   if (!ignored) {
       ignored = ( { } );
+   }
    return ignored;
 }
 
@@ -355,8 +361,9 @@ void message(string str, varargs int chat_flag) {
    if (!this_object()->query_user()) {
       return;
    }
-   if (this_object()->is_snooped())
+   if (this_object()->is_snooped()) {
       this_object()->do_snoop(str);
+   }
 
    if (chat_flag) {
       this_object()->query_user()->wrap_message(str, 1);
@@ -371,8 +378,9 @@ void message_orig(string str) {
       return;
    }
 
-   if (this_player()->is_snooped())
+   if (this_player()->is_snooped()) {
       this_player()->do_snoop(str);
+   }
    query_user()->put_message(str);
 }
 
@@ -429,32 +437,37 @@ void more(string * lines, varargs int docolor) {
    }
 
    height = query_env("height");
-   if (height == nil)
+   if (height == nil) {
       height = 23;
-   else if (stringp(height))
+   } else if (stringp(height)) {
       height = str2val(height);
+   }
 
-   if (height == -1)
+   if (height == -1) {
       height = 23;
-   if (height == 0)
+   } else if (height == 0) {
       height = INT_MAX;
-   if (height > 1)
+   }
+
+   if (height > 1) {
       height = height - 1;
+   }
 
    more_line_num = 0;
    more_lines = lines;
 
    if (sizeof(lines) > height + more_line_num) {
 
-   msg = implode(lines[more_line_num..more_line_num + height], "\n");
-   if (docolor) {
-      this_object()->query_user()->wrap_message(msg);
-   } else {
-      out_unmod(msg + "\n");
-   }
+      msg = implode(lines[more_line_num..more_line_num + height], "\n");
+
+      if (docolor) {
+         this_object()->query_user()->wrap_message(msg);
+      } else {
+         out_unmod(msg + "\n");
+      }
 
       out("%^BOLD%^--More--(" + ((more_line_num +
-	       height) * 100) / sizeof(lines) + "%)%^RESET%^");
+         height) * 100) / sizeof(lines) + "%)%^RESET%^");
       more_line_num += height + 1;
       input_to("more_prompt");
    } else {
@@ -478,8 +491,9 @@ void more_prompt(string arg) {
    string msg;
    mixed height;
 
-   if (!arg || arg == "")
+   if (!arg || arg == "") {
       arg = " ";
+   }
    switch (arg[0]) {
       case 'q':
       case 'Q':
@@ -494,17 +508,21 @@ void more_prompt(string arg) {
 
    height = query_env("height");
 
-   if (height == nil)
+   if (height == nil) {
       height = 23;
-   else if (stringp(height))
+   } else if (stringp(height)) {
       height = str2val(height);
+   }
 
-   if (height == -1)
+   if (height == -1) {
       height = 23;
-   else if (height == 0)
+   } else if (height == 0) {
       height = INT_MAX;
-   if (height > 1)
+   }
+
+   if (height > 1) {
       height = height - 1;
+   }
 
    if (sizeof(more_lines) > height + more_line_num) {
       msg = implode(more_lines[more_line_num..more_line_num + height], "\n");
@@ -520,6 +538,7 @@ void more_prompt(string arg) {
       input_to("more_prompt");
    } else {
       msg = implode(more_lines[more_line_num..], "\n");
+
       if (color_more) {
          this_object()->query_user()->wrap_message(msg);
       } else {
@@ -542,8 +561,9 @@ void do_look(int brief) {
 
    this_environment()->event("body_look", this_player());
    write(" ");
-   if (query_env("show_location"))
+   if (query_env("show_location")) {
       write("%^BOLD%^" + query_environment()->file_name() + "%^RESET%^");
+   }
    write(this_environment()->query_desc(0));
 
    set_this_player(save_player);
@@ -555,8 +575,9 @@ void do_go(string dir) {
 
    error = this_environment()->body_exit(this_player(), dir);
 
-   if (error)
+   if (error) {
       write(error);
+   }
 }
 
 void do_quit(void) {
@@ -576,8 +597,9 @@ void do_quit(void) {
 
    objs = query_inventory() + ( { } );
 
-   if (is_possessing())
+   if (is_possessing()) {
       call_other("/cmds/wiz/possess", "main", "");
+   }
 
    for (i = 0; i < sizeof(objs); i++) {
       if (objs[i]->is_undroppable() || autoload == 1) {
@@ -606,7 +628,9 @@ void do_quit(void) {
       object room;
 
       room = this_player()->query_environment();
-      if (room) set_env("start", room->file_name());
+      if (room) { 
+         set_env("start", room->file_name());
+      }
    }
 
    quitcmd = this_player()->query_env("quit_message");
@@ -636,43 +660,52 @@ void destruct(void) {
 /* Alias handling */
 
 int is_alias(string cmd) {
-   if (!alias)
+   if (!alias) {
       alias = ([]);
-   if (alias[cmd])
+   }
+   if (alias[cmd]) {
       return 1;
+   }
    return 0;
 }
 
 string query_alias(string cmd) {
-   if (!alias)
+   if (!alias) {
       alias = ([]);
+   }
    return alias[cmd];
 }
 
 void add_alias(string cmd, string new_alias) {
-   if (!alias)
+   if (!alias) {
       alias = ([]);
+   }
    alias[cmd] = new_alias;
 }
 
 void remove_alias(string cmd) {
-   if (!alias)
+   if (!alias) {
       alias = ([]);
+   }
 
    alias[cmd] = nil;
 }
 
 mapping query_aliases(void) {
-   if (!alias)
+   if (!alias) {
       alias = ([]);
+   }
+
    return alias;
 }
 
 /* Guild routines */
 
 void join_guild(string guild) {
-   if (!guilds)
+   if (!guilds) {
       guilds = ([]);
+   }
+
    guilds[guild] = GUILD_D->query_guild_title(guild);
    cmd_path += ( {
       "/cmds/guild/" + guild}
@@ -681,8 +714,10 @@ void join_guild(string guild) {
 }
 
 void leave_guild(string guild) {
-   if (!guilds)
+   if (!guilds) {
       guilds = ([]);
+   }
+
    guilds[guild] = nil;
    cmd_path -= ( {
       "/cmds/guild/" + guild}
@@ -692,10 +727,13 @@ void leave_guild(string guild) {
 }
 
 int guild_member(string guild) {
-   if (!guilds)
+   if (!guilds) {
       guilds = ([]);
-   if (guilds[guild])
+   }
+
+   if (guilds[guild]) {
       return 1;
+   }
    return 0;
 }
 
@@ -741,8 +779,9 @@ void receive_message(string message) {
 
    arg = "";
 
-   if (this_player()->is_snooped())
+   if (this_player()->is_snooped()) {
       this_player()->do_snoop(message);
+   }
 
    /* Redirect the input somewhere else */
    if (input_to_func != "") {
@@ -756,16 +795,20 @@ void receive_message(string message) {
       string temp;
       /* Expand the command */
       temp = ALIAS_D->expand_alias(message);
-      if (temp)
+      if (temp) {
 	 message = temp;
+      }
+
       /* Split the input into command and argument */
-      if (sscanf(message, "%s %s", cmd, arg) != 2)
+      if (sscanf(message, "%s %s", cmd, arg) != 2) {
 	 cmd = message;
+      }
 
       if (is_alias(cmd)) {
 	 message = ALIAS_D->do_expand(query_alias(cmd), arg);
-	 if (sscanf(message, "%s %s", cmd, arg) != 2)
+	 if (sscanf(message, "%s %s", cmd, arg) != 2) {
 	    cmd = message;
+         }
       }
 
       if (cmd != "") {
@@ -787,8 +830,9 @@ void receive_message(string message) {
       }
 
       /* Substitute 'me' with my name */
-      if (arg == "me")
+      if (arg == "me") {
 	 arg = this_player()->query_id();
+      }
 
       /* Check for an object command in objects in my inv */
       if (!flag) {
@@ -799,6 +843,7 @@ void receive_message(string message) {
          if (player) {
             object *objs;
             int y, maxy;
+
             objs = player->query_inventory();
             if (objs) {
                maxy = sizeof(objs);
@@ -839,6 +884,7 @@ void receive_message(string message) {
          if (room) {
             object *objs;
             int y, maxy;
+
             objs = room->query_inventory();
             if (objs) {
                maxy = sizeof(objs);
@@ -858,6 +904,7 @@ void receive_message(string message) {
       if (!flag) {
 	 object room;
 	 string roomcmd_h;
+
          room = this_environment();
          if (room) {
 	    roomcmd_h = room->query_room_command(cmd);
@@ -974,8 +1021,9 @@ void set_custom_color(string name, string * symbols) {
    string tmp;
 
    tmp = "";
-   if (!custom_colors)
+   if (!custom_colors) {
       custom_colors = ([]);
+   }
 
    if (!symbols) {
       custom_colors[name] = nil;
@@ -1032,10 +1080,11 @@ int query_muzzle() {
 }
 
 int toggle_muzzle() {
-   if (muzzle)
+   if (muzzle) {
       muzzle = 0;
-   else
+   } else {
       muzzle = 1;
+   }
 
    return muzzle;
 }
