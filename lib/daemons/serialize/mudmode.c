ba069a310877dc3f107ff1c8977df2ecc062d9a0
Kent Mein <mein@cs.umn.edu>
2013-08-05 10:32:44 -0500

    Update by Aidil to make it more efficent.

--- lib/daemons/serialize/mudmode.c
+++ lib/daemons/serialize/mudmode.c
@@ -18,230 +18,204 @@
  * February 2009        Version 2.0     Bart van Leeuwen
  *      # Rewritten restore_variable()
  *      # Fix implode/explode related bugs in save_variable()
+ *
+ * August 2013          Version 2.1     Bart van Leeuwen
+ *      # New grammar for restore_variable
  */
 
 #include        <type.h>
 
 #define GRAMMAR \
-  "escaped=/\\\\/\n"+\
-  "quote=/\"/\n"+\
-  "floats=/[\-]?[0-9]*[.][0-9]+/\n"+\
-  "arraystart=/[(][{][ ]*/\n"+\
-  "arrayend=/[ ]*[}][)]/\n"+\
-  "arraysep=/[ ]*,[ ]*/\n"+\
-  "mapstart=/[(][[][ ]*/\n"+\
-  "mapend=/[ ]*][)]/\n"+\
-  "mapsep=/[ ]*:[ ]*/\n"+\
-  "ints=/[\-]?[0-9]+/\n"+\
-  "char=/./\n"+\
-  "var: float\n"+\
-  "var: int\n"+\
-  "var: string\n"+\
-  "var: array\n"+\
-  "var: map\n"+\
-  "string: quote quote ? emptystring\n"+\
-  "string: quote chars quote ? concatstring\n"+\
-  "array: arraystart arrayend ? mkemptyarray\n"+\
-  "array: arraystart arrayelements arrayend ? mkarray\n"+\
-  "map: mapstart mapend ? mkemptymap\n"+\
-  "map: mapstart mapelements mapend ? mkmap\n"+\
-  "arrayelements: element\n"+\
-  "arrayelements: arrayelements arraysep element\n"+\
-  "arrayelements: arrayelements arraysep\n"+\
-  "element: int\n"+\
-  "element: float\n"+\
-  "element: string\n"+\
-  "element: array\n"+\
-  "element: map\n"+\
-  "mapelements: element mapsep element\n"+\
-  "mapelements: mapelements arraysep element mapsep element\n"+\
-  "mapelements: mapelements arraysep\n"+\
-  "chars: char\n"+\
-  "chars: escaped escaped\n"+\
-  "chars: escaped quote\n"+\
-  "chars: arraystart\n"+\
-  "chars: arrayend\n"+\
-  "chars: mapstart\n"+\
-  "chars: mapend\n"+\
-  "chars: arraysep\n"+\
-  "chars: mapsep\n"+\
-  "chars: floats\n"+\
-  "chars: ints\n"+\
-  "chars: chars char\n"+\
-  "chars: chars escaped escaped\n"+\
-  "chars: chars escaped quote\n"+\
-  "chars: chars arraystart\n"+\
-  "chars: chars arrayend\n"+\
-  "chars: chars mapstart\n"+\
-  "chars: chars mapend\n"+\
-  "chars: chars arraysep\n"+\
-  "chars: chars mapsep\n"+\
-  "chars: chars floats\n"+\
-  "chars: chars ints\n"+\
-  "int: ints ? cnvint\n"+\
-  "float: floats ? cnvfloat\n"
+  "string = /\"([^\\\\\"\n]*(\\\\.)*)*\"/\n"+\
+  "int = /-?[0-9]*/"+\
+  "float = /-?[0-9]*\\.[0-9]+/\n"+\
+  "float = /-?[0-9]+\\.[0-9]*([eE][-+]?[0-9]+)/\n"+\
+  "float = /-?[0-9]*\\.[0-9]+([eE][-+]?[0-9]+)/\n"+\
+  "result: data\n"+\
+  "data: \n"+\
+  "data: var\n"+\
+  "var: string ? cnvstring\n"+\
+  "var: map\n ? mkmap\n"+\
+  "var: array ? mkarray\n"+\
+  "var: int ? cnvint\n"+\
+  "var: float ? cnvfloat\n"+\
+  "map: '(' '[' opt_melements ']' ')'\n"+\
+  "array: '(' '{' opt_aelements '}' ')'\n"+\
+  "melement: var ':' var\n"+\
+  "opt_melements: \n"+\
+  "opt_melements: melements\n"+\
+  "opt_melements: melements ','\n"+\
+  "melements: melement\n"+\
+  "melements: melements ',' melement\n"+\
+  "opt_aelements: \n"+\
+  "opt_aelements: aelements\n"+\
+  "opt_aelements: aelements ','\n"+\
+  "aelements: var\n"+\
+  "aelements: aelements ',' var\n"
+
 
 /*
- * Conversion functions used by the restore_variable grammar
+ * Conversion functions used by the above grammar
  */
-static int *cnvint(mixed * data) {
-   int r;
 
-   sscanf(data[0], "%d", r);
-   return ( { r } );
-}
-
-static float *cnvfloat(mixed * data) {
-   float r;
-
-   sscanf(data[0], "%f", r);
-   return ( { r } );
-}
-
-static object *cnvobj(mixed * data) {
-   string n;
-   int i;
-
-   n = "";
-   for (i = 0; i < sizeof(data[0]); i++) {
-      n += data[0][i];
-   }
-   return ( { find_object(n) } );
-}
-
-static string *emptystring(mixed * data) {
-   return ( { "" } );
-}
-
-static string *concatstring(mixed * data) {
-   int i;
-   string r;
-
-   r = "";
-
-   for (i = 0; i < sizeof(data); i++) {
-      if (data[i] == "\\")
-	 i++;
-      r += data[i];
-   }
 
 /*
- * It seems this is not needed
+ * convert int litteral into an int
  *
-  r = implode(explode(r,"\\\""),"\"");
-  r = implode(explode(r,"\\\\"),"\\");
-*/
-   return ( { r[1..strlen(r) - 2] } );
+ */
+static int * cnvint(mixed * data) {
+    int r;
+
+    sscanf(data[0],"%d",r);
+    return ({ r });
 }
 
-static mixed *mkemptyarray(mixed * data) {
-   return ( { ( { } ) } );
+/*
+ * convert float litteral into a float
+ *
+ */
+static float * cnvfloat(mixed * data) {
+    float r;
+
+    sscanf(data[0],"%f",r);
+    return ({ r });
 }
 
-static mixed *mkarray(mixed * data) {
-   int i;
-   mixed *stuff;
+/*
+ * Convert string encoded object reference into an object
+ *
+ */
+static object * cnvobj(mixed * data) {
+    string n;
+    int i;
 
-   stuff = ( { } );
-   for (i = 1; i < sizeof(data) && data[i] != "})"; i += 2) {
-      stuff += ( { data[i] } );
-   }
-   return ( { stuff } );
+    n = "";
+    for(i=0;i<sizeof(data[0]);i++) {
+	n += data[0][i];
+    }
+    return ({ find_object(n) });
 }
 
-static mixed *mkemptymap(mixed * data) {
-   return ( { ([ ]) } );
+/*
+ * Convert string litteral into a string
+ *
+ */
+static string * cnvstring(mixed * data) {
+    string r;
+
+    r = data[0];
+
+    r = implode(explode(r,"\\\""),"\"");
+    r = implode(explode(r,"\\\\"),"\\");
+
+    return ({ r[1..strlen(r)-2] });
 }
 
-static mixed *mkmap(mixed * data) {
-   int i;
-   mapping stuff;
-   mixed key, val;
+/*
+ * Convert array litteral into an array
+ *
+ */
+static mixed * mkarray(mixed * data) {
+    int i;
+    mixed *stuff;
 
-   stuff = ([]);
-   for (i = 1; i < sizeof(data) && data[i] != "])"; i += 4) {
-      key = data[i];
-      val = data[i + 2];
-      stuff[key] = val;
-   }
-   return ( { stuff } );
+    stuff = ({ });
+    for(i=2; i<sizeof(data)-2; i += 2) {
+	stuff += ({ data[i] });
+    }
+    return ({ stuff });
+}
+
+/*
+ * Convert mapping litteral into a mapping
+ *
+ */
+static mixed * mkmap(mixed * data) {
+    int i;
+    mapping stuff;
+    mixed key,val;
+
+    stuff = ([ ]);
+    for(i=2; i<sizeof(data)-1 && ((data[i]+data[i+1]) != "])"); i += 4) {
+	key = data[i];
+	val = data[i+2];
+	stuff[key] = val;
+    }
+    return ({ stuff });
 }
 
 mixed restore_value(string str) {
-   mixed *result;
+    mixed * result;
 
-   result = parse_string(GRAMMAR, str);
+    result = parse_string(GRAMMAR, str);
 
-   if (!result || sizeof(result) < 1) {
-      return nil;
-   } else {
-      return result[0];
-   }
+    if( !result || sizeof(result) < 1 ) {
+	return nil;
+    } else {
+	return result[0];
+    }
 }
 
 string save_value(mixed var) {
-   string result;
-   int i, s;
-   mixed *keys, *values;
-   string pref, post;
+    string       result;
+    int          i, s;
+    mixed        *keys, *values;
+    string pref,post;
 
-   if (!var) {
-      return "0";
-   }
-   switch (typeof(var)) {
-      case T_INT:
-      case T_FLOAT:
-	 result = "" + var;
-	 break;
-      case T_STRING:
-
-	 if (strlen(var) > 0) {
-	    if (var[0..0] == "\\") {
-	       pref = "\\\\";
-	    } else if (var[0..0] == "\"") {
-	       pref = "\\\"";
+    if (!var) {
+	return "0";
+    }
+    switch (typeof(var)) {
+    case T_INT : case T_FLOAT :
+	result = ""+var;
+	break;
+    case T_STRING :
+	if(strlen(var) > 0) {
+	    if(var[0..0] == "\\") {
+		pref = "\\\\";
+	    } else if(var[0..0] == "\"") {
+		pref = "\\\"";
 	    } else {
-	       pref = "";
+		pref = "";
 	    }
 
-	    if (var[strlen(var) - 1] == '\\') {
-	       post = "\\\\";
-	    } else if (var[strlen(var) - 1] == '\"') {
-	       post = "\\\"";
+	    if(var[strlen(var)-1] == '\\') {
+		post = "\\\\";
+	    } else if(var[strlen(var)-1] == '\"') {
+		post = "\\\"";
 	    } else {
-	       post = "";
+		post = "";
 	    }
 
 	    result = implode(explode(var, "\\"), "\\\\");
 	    result = implode(explode(result, "\""), "\\\"");
-	    result = "\"" + pref + result + post + "\"";
-	 } else {
+	    result = "\""+pref+result+post+"\"";
+	} else {
 	    result = "\"\"";
-	 }
-	 break;
-      case T_OBJECT:
-	 result = object_name(var);
-	 break;
-      case T_ARRAY:
-	 result = "({";
-	 for (i = 0, s = sizeof(var); i < s; i++) {
+	}
+	break;
+    case T_OBJECT :
+	result = object_name(var);
+	break;
+    case T_ARRAY :
+	result = "({";
+	for (i = 0, s = sizeof(var); i < s; i++) {
 	    result += save_value(var[i]) + ",";
-	 }
-	 result += "})";
-	 break;
-      case T_MAPPING:
-	 keys = map_indices(var);
-	 values = map_values(var);
-	 result = "([";
-	 for (i = 0, s = map_sizeof(var); i < s; i++) {
-	    result += save_value(keys[i]) + ":" + save_value(values[i])
-	       + ",";
-	 }
-	 result += "])";
-	 break;
-      default:
-	 result = nil;
-	 break;
-   }
-   return result;
+	}
+	result += "})";
+	break;
+    case T_MAPPING :
+	keys = map_indices(var);
+	values = map_values(var);
+	result = "([";
+	for (i = 0, s = map_sizeof(var); i < s; i++) {
+	    result += save_value(keys[i]) + ":" + save_value(values[i]) + ",";
+	}
+	result += "])";
+	break;
+    default :
+	result = nil;
+	break;
+    }
+    return result;
 }
