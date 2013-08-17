c5f37ecaba82648f691891abc7353bb8b4e7c8d6
Aidil <aidil@wotf.org>
2013-08-17 14:07:04 +0200

    Fix for alias_d to only check the 1st 10 arguments Fix routerlist in imud_d

--- lib/daemons/alias_d.c
+++ lib/daemons/alias_d.c
@@ -128,14 +128,14 @@ string query_alias(string cmd) {
 
 string do_expand(string alias, string arg) {
    string *args;
-   int i;
+   int i, sz;
 
    alias = replace_string(alias, "$*", arg);
 
    if (arg && arg != "") {
       args = explode(arg, " ");
 
-      for (i = 0; i < sizeof(args); i++) {
+      for (i = 0, sz = sizeof(args), sz=(sz<10 ? sz:10); i < sz; i++) {
 	 alias = replace_string(alias, "$" + i, args[i]);
       }
    }
