7ebae209be6fe96f0713003a652470d3ea1e2845
Kent Mein <mein@cs.umn.edu>
2013-08-21 19:26:31 -0500

    Renamed the first test monster.

---
+++ lib/domains/test/monsters/test1.c
@@ -0,0 +1,15 @@
+inherit "/std/monster";
+
+void setup() {
+   set_name("rat");
+   /* Leave out the gender specification, get a random one */
+   /*  set_gender( "male" ); */
+   add_adj("filthy");
+   set_short("A small filthy rat");
+   set_long("The rat smells awful.");
+   set_race("rodent");
+   set_level(1);
+   set_hit_skill("combat/unarmed");
+   set_skill("combat/unarmed", 50);
+   set_skill("combat/defense", 100);
+}
