3a5a07202c288205cb1760faf704fc8f84e119c0
Kent Mein <mein@cs.umn.edu>
2013-08-31 11:37:13 -0500

    Small patch by Aidil, to allow map_array to take a filename or an object instead of just an object.

--- lib/kernel/lib/afun/map_array.c
+++ lib/kernel/lib/afun/map_array.c
@@ -1,9 +1,11 @@
-static mixed *map_array(mixed * arr, string fun, object ob, varargs mixed extra) {
+static mixed *map_array(mixed * arr, string fun, mixed ob, 
+   varargs mixed extra) {
    mixed *result;
    int i, sz;
 
    argcheck(arrayp(arr), 1, "array");
    argcheck(stringp(fun), 2, "string");
+   argcheck((stringp(ob) || objectp(ob)), 3, "object or string");
 
    sz = sizeof(arr);
    result = allocate(sz);
