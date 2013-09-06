505658d458eeac6144e6fe0290cb9426956f5325
Kent Mein <mein@cs.umn.edu>
2013-09-06 00:50:24 -0500

    Added usage function for rehash command.  Still needs a little work.

--- lib/sys/cmds/admin/rehash.c
+++ lib/sys/cmds/admin/rehash.c
@@ -1,6 +1,28 @@
 inherit M_COMMAND;
 
-static void main( string arg ) {
-   COMMAND_D->rehash();
-   write("Ok.\n");
+void usage() {
+   string *lines;
+
+   lines = ({ "Usage: rehash [-h]" });
+   lines += ({ " " });
+   lines += ({ "Re evaluate the command paths to add/remove commands." });
+   lines += ({ " " });
+   lines += ({ "Options:" });
+   lines += ({ "\t-h\tHelp, this usage message." });
+   lines += ({ "Examples:" });
+   lines += ({ "\trehash" });
+   lines += ({ "See also:" });
+   lines += ({ "\tXXX copy cmdadm usage and add this one to related ones" });
+
+   this_player()->more(lines);
+}
+
+static void main(string str) {
+   if (str && str != "") {
+      usage();
+   } else {
+      COMMAND_D->rehash();
+      write("Ok.\n"); /* Would be nice for rehash to return # of commands or
+        something similar to print out for feedback XXX */
+   }
 }
