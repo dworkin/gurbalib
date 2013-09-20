48793206acd8f964f328ae23d239a4c3397c0ca7
Kent Mein <mein@cs.umn.edu>
2013-09-19 22:05:20 -0500

    Fix the pray command so it only shimmers if you are dead.

--- lib/cmds/player/pray.c
+++ lib/cmds/player/pray.c
@@ -21,22 +21,22 @@ static void main(string str) {
    object obj;
 
    if (!str || (str == "")) {
-      write("You pray and feel a shimmering light warm your entire body.");
+      write("You pray.\n");
       this_player()->query_environment()->tell_room(this_player(), 
-         this_player()->query_Name() + " prays.\nA shimmering light " +
-         "envelopes them.");
+         this_player()->query_Name() + " prays.\n");
    } else if (sscanf(str, "-%s", str)) {
       usage();
       return;
    } else {
-      write("You pray: " + str + 
-         "\nYou feel a shimmering light warm your entire body.");
+      write("You pray: " + str + "\n");
       this_player()->query_environment()->tell_room(this_player(), 
-         this_player()->query_Name() + " prays: " + str  +
-         "\nA shimmering light envelopes them.");
+         this_player()->query_Name() + " prays: " + str + "\n");
    }
 
    if (this_player()->is_dead()) {
+      write("You feel a shimmering light warm your entire body.");
+      this_player()->query_environment()->tell_room(this_player(), 
+         "A shimmering light envelopes " + this_player()->query_Name() + ".");
       this_player()->set_hp(1);
    }
 }
