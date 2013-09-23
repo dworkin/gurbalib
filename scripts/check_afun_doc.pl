2420b0447c63c5f607cc3f55a0d5138acd04c1a9
Kent Mein <mein@cs.umn.edu>
2013-09-22 23:18:37 -0500

    Make the script check afun-game as well.

--- scripts/check_afun_doc.pl
+++ scripts/check_afun_doc.pl
@@ -1,20 +1,26 @@
 #!/usr/bin/perl
 
-my (@files, $fh, $x, $tmp, $test); 
-my $DIR = "./lib/kernel/lib/afun";
+sub check_dir {
+   my ($dir) = @_;
+   my (@files, $fh, $x, $tmp, $test); 
 
-opendir($fh, $DIR) or die "Unable to open $DIR\n";;
-@files = readdir($fh);
-close $fh;
+   opendir($fh, $dir) or die "Unable to open $dir\n";;
+   @files = readdir($fh);
+   close $fh;
 
-foreach my $i (@files) {
-	if ($i =~ /^.*\.c$/) {
-	   $x = length($i);
-           $tmp = substr($i,0,$x-2);
-	   $test = "./lib/doc/afun/$tmp";
-	   if (-f "./lib/doc/afun/$tmp") {
-	   } else {
-		print "Warning no doc: $test\n";
-           }
-        }
+   foreach my $i (@files) {
+      if ($i =~ /^.*\.c$/) {
+         $x = length($i);
+         $tmp = substr($i,0,$x-2);
+         $test = "./lib/doc/afun/$tmp";
+
+         if (-f "./lib/doc/afun/$tmp") {
+         } else {
+            print "Warning no doc: $test\n";
+         }
+      }
+   }
 }
+
+check_dir("./lib/kernel/lib/afun");
+check_dir("./lib/kernel/lib/afun-game");
