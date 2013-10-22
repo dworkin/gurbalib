b2bfc5cad5ee5a3e2c8ae741763ca86ef9b50c51
Kent Mein <mein@cs.umn.edu>
2013-10-22 00:31:38 -0500

    More work on the ban daemon.

--- lib/sys/daemons/banish_d.c
+++ lib/sys/daemons/banish_d.c
@@ -20,40 +20,46 @@ string *query_banished_names() {
 }
 
 int is_banished(string name) {
-   if (member_array(name, banished_names) != -1)
+   if (member_array(name, banished_names) != -1) {
       return 1;
+   }
 
    return 0;
 }
 
 int banish_name(string str) {
-   if (!require_priv("wizard"))
+   if (!require_priv("wizard")) {
       error("Illegal use of banish_d");
-
-   if (str == "")
       return 0;
+   }
+
+   if (str == "") {
+      return 0;
+   }
 
    str = lowercase(str);
 
    if (file_exists(BAN_DIR + str) ||
       (!unguarded("write_file", BAN_DIR + str, ctime(time()) + "\tby:  " +
-	    this_player()->query_name() + "\n")))
+         this_player()->query_name() + "\n"))) {
       return 0;
+   }
 
    create();
    return 1;
 }
 
 int unbanish_name(string str) {
-   if (!require_priv("wizard"))
+   if (!require_priv("wizard")) {
       error("Illegal use of banish_d");
+      return 0;
+   }
 
    str = lowercase(str);
 
    if (file_exists(BAN_DIR + str)) {
       unguarded("remove_file", BAN_DIR + str);
-      create();
-      return 1;
    }
-   return 0;
+   create();
+   return 1;
 }
