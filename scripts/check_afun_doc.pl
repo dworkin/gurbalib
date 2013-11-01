fae28a982afe408677f99f5af2771802b6b065c7
Kent Mein <mein@cs.umn.edu>
2013-10-31 23:25:00 -0500

    Update script to check for basic Headers.
    Will expand this later to do more checks, for now there is still a lot
    of work to clean up so this is enough for now.

--- scripts/check_afun_doc.pl
+++ scripts/check_afun_doc.pl
@@ -1,5 +1,32 @@
 #!/usr/bin/perl
 
+sub file_contains {
+	my ($file, $line) = @_;
+
+	my $tmp = `grep $line \"$file\"`;
+	chomp $tmp;
+	if ($tmp && (length($tmp) > 1)) {
+		return 1;
+	}
+	return 0;
+}
+
+sub check_file {
+	my ($file) = @_;
+	foreach my $i ("NAME", "SYNOPSIS", "DESCRIPTION", "RETURN", 
+		"EXAMPLES", "SEE") {
+		if (!file_contains($file,$i)) {
+			if ($i eq "SEE") {
+				print("$file is missing SEE ALSO\n");
+			} elsif ($i eq "RETURN") {
+				print("$file is missing RETURN VALUES\n");
+			} else {
+				print("$file is missing $i\n");
+			}
+		}
+	}
+}
+
 sub check_dir {
    my ($dir) = @_;
    my (@files, $fh, $x, $tmp, $test); 
@@ -14,7 +41,8 @@ sub check_dir {
          $tmp = substr($i,0,$x-2);
          $test = "./lib/doc/afun/$tmp";
 
-         if (-f "./lib/doc/afun/$tmp") {
+         if (-f $test) {
+		check_file($test);
          } else {
             print "Warning no doc: $test\n";
          }
