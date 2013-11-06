7352be317e12ac7b95e41dcf6430aaa0157fb591
Kent Mein <mein@cs.umn.edu>
2013-11-06 15:22:19 -0600

    Fix a typo.

--- lib/domains/2.4.5/rooms/maze1/maze5.c
+++ lib/domains/2.4.5/rooms/maze1/maze5.c
@@ -6,7 +6,7 @@ void setup(void) {
    add_area("2.4.5");
 
    set_short("End of the maze");
-   set_long("Congratulations you have done it.);
+   set_long("Congratulations you have done it.");
 
    add_exit("south", DIR + "/rooms/maze1/maze4.c");
 
