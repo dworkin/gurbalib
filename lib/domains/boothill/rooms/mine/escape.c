49d43ce89c086da8685a66ed5bd9c0f93b84920b
Kent Mein <mein@cs.umn.edu>
2014-01-27 17:46:31 -0600

    More work on the area doc.

--- lib/domains/boothill/rooms/mine/escape.c
+++ lib/domains/boothill/rooms/mine/escape.c
@@ -10,8 +10,25 @@ void setup(void) {
       "light at the end of the tunnel.  There is a hole to the east.  " +
       "To the west the mine crawls deeper into the mountain.");
 
+   add_object_command("move", "move_rubble");
+
+   add_item("rubble", "The wall has a bunch of rocks.  It looks like " +
+      "you might be able to move it.");
+
    set_exits(([
       "west" : DIR + "/rooms/mine/mineshaft.c",
    ]));
 }
 
+int move_rubble(string str) {
+   if (str == "rubble") {
+      write("You rummage around in the rubble and slide through to " +
+         "the other side.");
+      this_enviornment()->tell_room(this_player(), this_player()->query_Name() +
+         " digs through the rubble and dissapears.");
+
+      this_player()->move(DIR + "/rooms/mntpath.c");
+      return 1;
+   } 
+   return 0;
+}
