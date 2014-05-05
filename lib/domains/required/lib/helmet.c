36b2b1a302c8c0e62bcc2f187e5cfa8d669866a2
november kilo <natasha.i.koroleva@gmail.com>
2014-05-05 13:13:45 -0500

    add a helmet lib; can be done for other armours.

---
+++ lib/domains/required/lib/helmet.c
@@ -0,0 +1,16 @@
+inherit "/std/armor";
+
+void setup(void) {
+   set_id("helmet");
+   set_short("A helmet");
+   set_long("A helmet. This is very basic advenuterer's gear. " +
+		"You better find something else.");
+   set_gettable(1);
+
+   set_ac(1);
+
+   set_slot("head");
+   set_value(20);
+   set_size(10);
+   set_weight(7);
+}
--- lib/domains/required/objects/helmet.c
+++ lib/domains/required/objects/helmet.c
@@ -1,17 +1,11 @@
-inherit "/std/armor";
+#include "../domain.h"
+
+inherit DIR + "/lib/helmet";
 
 void setup(void) {
-   set_id("helmet");
+	::setup();
    set_adj("small", "round");
    set_short("A small rusty helmet");
    set_long("A small rusty helmet, it looks like it has seen better days.  " +
       "You would have to be really desperate to put this thing on.");
-   set_gettable(1);
-
-   set_ac(1);
-
-   set_slot("head");
-   set_value(20);
-   set_size(10);
-   set_weight(7);
 }
