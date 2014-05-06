772ab7e88f67a05eac77746a26543473beeb973e
november kilo <natasha.i.koroleva@gmail.com>
2014-05-06 17:01:11 -0500

    just in case we are not using the cliffs, don't add the exit to there.

--- lib/domains/2.4.5/rooms/mount_top2.c
+++ lib/domains/2.4.5/rooms/mount_top2.c
@@ -2,16 +2,26 @@
 
 inherit "/std/room";
 
+#ifdef USE_NOKICLIFFS
+string query_long() {
+	string str;
+	str = ::query_long();
+	str += " To the east the legendary Noki Cliffs loom large.";
+	return str;
+}
+#endif
+
 void setup(void) {
    add_area("2.4.5");
 
    set_short("Plateau");
    set_long("You are on a large, open plateau on top of the mountain.  " +
       "The view is fantastic in all directions and the clouds that " +
-      "rush past above, feel so close you could almost touch them.  " +
-      "The air here is fresh and clean. To the east the legendary " +
-		"Noki Cliffs loom large.");
+      "rush past above, feel so close you could almost touch them. " +
+      "The air here is fresh and clean.");
 
+#ifdef USE_NOKICLIFFS
 	add_exit("east", "/domains/nokicliffs/rooms/start.c");
+#endif
    add_exit("west", DIR + "/rooms/mount_top.c");
 }
