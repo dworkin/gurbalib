e0ebb429f9ac8656efd854b659734c8fe633db6b
november kilo <natasha.i.koroleva@gmail.com>
2014-05-09 22:56:37 -0500

    fix bug in add_coins().

--- lib/std/monster.c
+++ lib/std/monster.c
@@ -168,6 +168,9 @@ void equip_monster(string *arms, varargs int repeats_ok) {
 
 void add_coins(string type, int amt) {
 	object coins;
+	if (present("coins")) {
+		return;
+	}
 	if (amt < 1) {
 		amt = 1;
 	}
