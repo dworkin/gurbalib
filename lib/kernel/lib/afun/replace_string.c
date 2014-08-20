5912c2bd80d003afd348f2de3a428326a8a42fc7
Kent Mein <mein@cs.umn.edu>
2014-08-20 10:04:56 -0500

    Renamed a var so things compile again.

--- lib/kernel/lib/afun/replace_string.c
+++ lib/kernel/lib/afun/replace_string.c
@@ -1,10 +1,10 @@
-string replace_string(string str, string old, string new) {
+string replace_string(string str, string old, string newstr) {
    int d, olen, slen, i;
    string done;
 
    argcheck(str, 1, "string");
    argcheck(old, 2, "string");
-   argcheck(new, 3, "string");
+   argcheck(newstr, 3, "string");
 
    olen = strlen(old) -1;
    slen = strlen(str);
@@ -17,8 +17,8 @@ string replace_string(string str, string old, string new) {
 	 /* Still not reached the end of the string */
 	 if (str[i..(i + olen)] == old) {
 	    /*Found an occurance of the old string */
-	    done += new;
-	    d += strlen(new);
+	    done += newstr;
+	    d += strlen(newstr);
 	    i += olen;
 	 } else {
 	    done += " ";
