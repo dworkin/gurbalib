051c3b82bdba57524aa8be81f9ce27a5b313d908
nullinfinite <plord.way@gmail.com>
2014-12-17 00:16:18 -0500

    added the say safun
    same thing as seen on older lpmuds

---
+++ lib/sys/safun/say.c
@@ -0,0 +1,25 @@
+void say(string str, varargs object obj) {
+   int i;
+   object room, player;
+   object *listeners;
+
+   argcheck(str, 1, "string");
+
+   if (!this_player() ) {
+      return;
+   }
+
+   if(obj) {
+      player = obj;
+      }
+   else {
+      player = this_player();
+   }
+
+   listeners = this_environment()->query_inventory();
+   for(i = 0; i < sizeof(listeners); i++) {
+      if(listeners[i]->is_living() && listeners[i] != player) {
+         listeners[i]->message(str);
+      }
+   }
+}
