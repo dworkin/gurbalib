56e47cca095f896c5acd980d891653d8126012ec
nullinfinite <plord.way@gmail.com>
2015-01-25 02:16:44 -0500

    fixed a bug in call_trace afun

--- lib/kernel/lib/afun-game/call_trace.c
+++ lib/kernel/lib/afun-game/call_trace.c
@@ -9,7 +9,7 @@ static mixed *call_trace(void) {
    trace =::call_trace();
    sz = sizeof(trace);
    trace = trace[0..(sz - 2)];
-   for (i = 0; i < sz; i++) {
+   for (i = 0; i < (sz-1); i++) {
       trace[i] = trace[i][0..(TRACE_FIRSTARG - 1)];
    }
    return trace;
