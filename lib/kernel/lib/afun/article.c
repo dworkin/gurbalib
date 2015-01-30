1df48290bc52125ccefb4e198218206ce632ffcd
nullinfinite <plord.way@gmail.com>
2015-01-30 12:21:09 -0500

    added more special exceptions to article fun

--- lib/kernel/lib/afun/article.c
+++ lib/kernel/lib/afun/article.c
@@ -3,8 +3,17 @@ string article(string name) {
 
    if ((strlen(name) > 3 && lowercase(name[0..3]) == "hour") ||
       (strlen(name) > 4 && lowercase(name[0..4]) == "honor") ||
-      (strlen(name) > 5 && lowercase(name[0..5]) == "honour")) {
+      (strlen(name) > 5 && ( (lowercase(name[0..5]) == "honour") || 
+      (lowercase(name[0..5]) == "honest") ) ) ) {
       return "an";
+   }
+   if ((strlen(name) > 3 && lowercase(name[0..2]) == "uni") ||
+      (strlen(name) > 4 && lowercase(name[0..4]) == "usual") ||
+      (strlen(name) > 5 && lowercase(name[0..5]) == "eunuch") ||
+      (strlen(name) > 6 && ( (lowercase(name[0..6]) == "ukelele") ||
+      (lowercase(name[0..6]) == "uranium" ) ||
+      (lowercase(name[0..6]) == "euphori" ) ) ) ) {
+      return "a";
    }
 
    switch (name[0]) {
