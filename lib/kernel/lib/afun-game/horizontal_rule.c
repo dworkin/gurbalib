b902386025d996bdb6786d6fdcb6821eb656fff9
nullinfinite <plord.way@gmail.com>
2015-02-10 20:36:55 -0500

    made horizontal rule work with longer str

--- lib/kernel/lib/afun-game/horizontal_rule.c
+++ lib/kernel/lib/afun-game/horizontal_rule.c
@@ -2,7 +2,7 @@ string horizontal_rule(object "/sys/obj/player" player, string str) {
 	int width, i;
 	string retval;
 	width = str2val((string) this_player()->query_env("width"));
-	for (retval = "", i = 0; i < width; i++) {
+	for (retval = "", i = 0; i < width; i += strlen(str) ) {
 		retval += str;
 	}
 	return retval;
