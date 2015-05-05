e90a1a3f401716826ca1387c4e36f7ae126af239
Kent Mein <mein@cs.umn.edu>
2015-05-05 09:30:02 -0500

    Fix some of the default objects and remove a TODO item.

--- lib/domains/required/objects/helmet.c
+++ lib/domains/required/objects/helmet.c
@@ -3,7 +3,8 @@
 inherit "/std/armor";
 
 void setup(void) {
-   set_adj("small", "round");
+   set_adj("small", "round", "rusty");
+   set_id("helmet");
    set_short("A small rusty helmet");
    set_long("A small rusty helmet, it looks like it has seen better days.  " +
       "You would have to be really desperate to put this thing on.");
--- lib/domains/required/objects/short_sword.c
+++ lib/domains/required/objects/short_sword.c
@@ -4,6 +4,7 @@ inherit "/std/weapon";
 
 void setup(void) {
    set_combat_stats(1, 3, 0);
+   set_id("sword");
    set_adj("short");
    set_short("A short sword");
    set_long("Your standard small sword.");
