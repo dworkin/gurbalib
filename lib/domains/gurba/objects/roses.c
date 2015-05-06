cabbc1e1c7448495a2f649ad3f6d33df8ded04fe
Kent Mein <mein@cs.umn.edu>
2015-05-06 10:44:32 -0500

    Small fix so checking the file works.

--- lib/domains/gurba/objects/roses.c
+++ lib/domains/gurba/objects/roses.c
@@ -5,7 +5,6 @@ inherit "/std/object";
 void setup(void) {
    set_id("bouquet", "flowers");
    set_adj("rose", "red");
-   set_short("Someone has left a bouquet of roses here.");
    set_long("A beautiful bouquet of red roses.");
    set_gettable(1);
    set_value(10);
@@ -13,8 +12,9 @@ void setup(void) {
 }
 
 string query_short(void) {
-   if (this_object()->query_environment()->is_living()) {
-      return "A bouquet of roses.";
+   if (this_object()->query_environment() && 
+      (this_object()->query_environment()->is_living())) {
+      return "A bouquet of roses";
    }
    return "Someone has left a bouquet of roses here.";
 } 
