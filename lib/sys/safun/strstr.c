c88e71ae806918c50501e347daaed4a6e94d677b
Kent Mein <mein@cs.umn.edu>
2015-05-27 12:02:06 -0500

    Better version of strstr provided by Bart

--- lib/sys/safun/strstr.c
+++ lib/sys/safun/strstr.c
@@ -1,16 +1,14 @@
-int strstr(string search, string what) {
-   int i, lw, ls, sz;
+int strstr(string search, string what) { 
+   string pre; 
 
-   argcheck(search, 1, "string");
-   argcheck(what, 2, "string");
+   argcheck(search, 1, "string"); 
+   argcheck(what, 2, "string"); 
 
-   ls = strlen(search);
-   lw = strlen(what) - 1;
-   sz = ls - lw;
-   for (i = 0; i < sz; i++) {
-      if (search[i..(i + lw)] == what) {
-	 return i;
-      }
-   }
-   return -1;
+   what = implode(rexplode(what,"%"),"%%"); 
+
+   if(sscanf(search, "%s"+what+"%*s", pre) > 0) { 
+      return strlen(pre); 
+   } else { 
+      return -1; 
+   } 
 }
