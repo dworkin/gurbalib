74c1a6c796cdd4c1d6c52d34074450405c97c9f0
Kent Mein <mein@cs.umn.edu>
2015-07-21 06:25:37 -0500

    Fixed return in the wrong location, also cleaned up the file a little.

--- lib/sys/daemons/linkdead_d.c
+++ lib/sys/daemons/linkdead_d.c
@@ -17,14 +17,14 @@ void remove_linkdead(object ob) {
 
 object *query_linkdead(void) {
    return linkdead;
-   }
+}
 
 int is_linkdead(object ob) {
    if (!linkdead) {
       linkdead = ({ });
-      }
+   }
    if (member_array(ob, linkdead) > -1 ) {
       return 1;
-   return 0;
    }
+   return 0;
 }
