2fc5c8849745aa14521c56e5be5d0555c2d58923
November Kilo <natasha.i.koroleva@gmail.com>
2015-08-04 20:48:56 -0500

    Improve the long descriptions of rooms in the cave.

--- lib/domains/nokicliffs/rooms/cliffs/cave/bust_chamber.c
+++ lib/domains/nokicliffs/rooms/cliffs/cave/bust_chamber.c
@@ -6,8 +6,13 @@ void setup(void) {
    ::setup();
 
    set_short("A cave passage in the Noki Cliffs filled with busts");
-   set_long("There are a number of busts here, you do not recognize " +
-      "any of them.");
+   set_long("This is a strange chamber indeed. The purpose is entirely " +
+      "unclear, but it seems to be kind of ancestor worship or possibly " +
+      "religious worship. The walls of the cave have been made smooth " +
+      "some unknown magic or technology. Various busts of people you " +
+      "do not recognize have been carved out of the smoothed over walls. " +
+      "There is no apparent pattern to their arrangement that you are " +
+      "able to discern.");
 
    set_exits(([
       "north" : DIR + "/rooms/cliffs/cave/puzzle_chamber.c",
