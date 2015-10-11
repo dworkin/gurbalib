b0397929655ecbd43b50af3920f7d8baad824b9d
Kent Mein <mein@cs.umn.edu>
2015-10-11 10:19:25 -0500

    Fix code so it works without CLOSURES_EXTENSION

--- lib/kernel/lib/afun/filter_mapping.c
+++ lib/kernel/lib/afun/filter_mapping.c
@@ -3,23 +3,35 @@ static mapping filter_mapping( mapping map, mixed call, varargs mixed arg... ) {
    mixed *idx;
    mapping result;
 
+#ifdef CLOSURES_EXTENSION
    argcheck( stringp( call ) || 
       functionp( call ), 1, "function pointer or function name" );
+#else
+   argcheck( stringp( call ), 1, "string" );
+#endif
 
    result = ([ ]);
    idx = map_indices( map );
 
    sz = sizeof(idx);
    for (i = 0; i < sz; i++ ) {
+
+#ifdef CLOSURES_EXTENSION
       if (functionp( call )) {
          if ((*call)( map[idx[i]], arg... )) {
             result[idx[i]] = map[idx[i]];
          }
       } else {
+#endif
+
          if (call_other( this_object(), call, map[idx[i]], arg... )) {
             result[idx[i]] = map[idx[i]];
          }
+
+#ifdef CLOSURES_EXTENSION
       }
+#endif
+
    }
 
    return result;
