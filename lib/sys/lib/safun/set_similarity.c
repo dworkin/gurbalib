5d1325ece7fc03aaefae54f677b5135f1d01c073
Kent Mein <mein@cs.umn.edu>
2016-03-23 14:47:40 -0500

    Added set_similarity safun
    This was provided by Aidil.

---
+++ lib/sys/lib/safun/set_similarity.c
@@ -0,0 +1,13 @@
+/* Calculate jaccard similarity between 2 sets
+   Returns a value in the range 0 (not similar at all) to 1 (very similar) */
+
+nomask static float set_similarity( mixed *a1, mixed *a2 ) {
+   argcheck( arrayp(a1), 1, "array" );
+   argcheck( arrayp(a2), 2, "array" );
+
+   if( !sizeof( a2 ) ) {
+      return (float) !sizeof( a1 );
+   }
+
+   return (float) sizeof( a1 & a2 ) / (float) sizeof( a1 | a2 );
+}
