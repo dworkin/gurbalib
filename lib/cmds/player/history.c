0c65e781ba4abb73be05551a948a976cdd2e092c
Kent Mein <mein@umn.edu>
2016-11-22 15:41:58 -0600

    Clean up the history command a little.
    Move the "man" info into the usage function similar to other commands.
    Make it so history with no args displays your current history which is more
    inline with unix usage.  Also expanded the help a little.

--- lib/cmds/player/history.c
+++ lib/cmds/player/history.c
@@ -6,17 +6,37 @@ inherit M_GETOPT;
 string *usage(void) {
    string *lines;
 
-   lines = ({ "Usage: history <-h|-r <range>>" });
+   lines = ({ "Usage: history [-h|-r [range]]" });
    lines += ({ "" });
-   lines += ({ "Use 'man cmd_history' to get more information. " });
+   lines += ({ "The last twenty successful commands are saved.  You can " +
+      "repeat any command " });
+   lines += ({ "in your history.  You may also remove commands from your " +
+      "history." });
+   lines += ({ "You can use the following shortcuts to repeate commands:" });
+   lines += ({ "!\tDisplay saved history." });
+   lines += ({ "!!\tRepeat the last command in your history." });
+   lines += ({ "!drink\tRepeat the last command in your history starting " +
+       "with \"drink\"" });
+   lines += ({ "" });
+   lines += ({ "If you want to remove commands from your saved history you " +
+      "can use the -r " });
+   lines += ({ "option.  The -r option also works with ranges." });
+   lines += ({ "To remove the third command use: history -r 3" });
+   lines += ({ "" });
+   lines += ({ "If no arguments are given you will get a display of your " +
+      "history." });
+   
    lines += ({ "" });
    lines += ({ "Options:" });
    lines += ({ "\t-h\tHelp, this usage message." });
    lines += ({ "\t-r\tRemove, removes a range of history." });
    lines += ({ "" });
    lines += ({ "Examples:" });
+   lines += ({ "\thistory" });
    lines += ({ "\thistory -h" });
    lines += ({ "\thistory -r 1-3,5,7-10" });
+   lines += ({ "\t!!" });
+   lines += ({ "\t!" });
 
    return lines;
 }
@@ -25,7 +45,10 @@ static void main(string str) {
    string opt;
    int *range, i, dim;
 
-   if (!empty_str(str) && sscanf(str, "-r %s", opt)) {
+   if (empty_str(str)) {
+	write(this_player()->list_history());
+	return;
+   } else if (sscanf(str, "-r %s", opt)) {
          range = parse_range(opt);
          dim = sizeof(range);
          for (i = 0; i < dim; i++) {
