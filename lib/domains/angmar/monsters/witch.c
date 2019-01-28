3c1b29e6137d0f61085ac0fc9972f6023b8ff539
november-kilo <natasha.i.koroleva@gmail.com>
2019-01-28 17:09:26 -0600

    found README, added; fix witch (#2)

--- lib/domains/angmar/monsters/witch.c
+++ lib/domains/angmar/monsters/witch.c
@@ -3,13 +3,13 @@
 inherit "/std/monster";
 
 void setup() {
-   set_name("scarecrow");
-   set_short("A scarecrow");
-   set_long("He is seeing to that nobody steals the corn...");
+   set_name("witch");
+   set_short("A witch");
+   set_long("The witches are not to be tampered with...");
    set_level(1);
    set_hit_skill("combat/unarmed");
    set_skill("combat/unarmed", 50);
    set_skill("combat/defense", 100);
 
-   equip_monster(({ DIR + "/armors/rubber_boots.c" }));
+   equip_monster(({ DIR + "/obj/weapons/broom.c" }));
 }
