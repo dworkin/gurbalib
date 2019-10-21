5567c215a165b0bb503ebdac43035e5190f2f387
Raymond Jennings <shentino@gmail.com>
2019-10-21 01:35:52 -0700

    Add rubies, also make different gems have different values

--- lib/domains/2.4.5/obj/gem.c
+++ lib/domains/2.4.5/obj/gem.c
@@ -5,24 +5,30 @@ inherit "/std/object";
 void setup(void) {
    int temp;
 
-   temp = random(3);
+   temp = random(4);
+
    if (temp == 0) {
       set_id("diamond");
-      add_ids("gem", "jewel");
       set_short("A diamond");
       set_long("A large cut diamond, it looks valuable.");
+      set_value(random(125) + 830);
    } else if (temp == 1) {
       set_id("emerald");
-      add_ids("gem", "jewel");
       set_short("An emerald");
       set_long("A large cut emerald, it looks valuable.");
+      set_value(random(125) + 230);
    } else if (temp == 2) {
       set_id("sapphire");
-      add_ids("gem", "jewel");
       set_short("A sapphire");
       set_long("A large cut sapphire, it looks valuable.");
+      set_value(random(125) + 130);
+   } else if (temp == 3) {
+      set_id("ruby");
+      set_short("A ruby");
+      set_long("A large cut ruby, it looks valuable.");
+      set_value(random(125) + 430);
    }
+   add_ids("gem", "jewel");
    set_gettable(1);
-   set_value(random(250) + 300);
    set_weight(1);
 }
