0f68fca1a3b4911e12802154e24ef24b5ef38fe1
evilmog <evilmog@Evils-MacBook-Pro.local>
2020-05-04 20:11:05 -0600

    added missing 'no pk' and 'no kill' code to rooms, must have been a partial commit

--- lib/cmds/player/consider.c
+++ lib/cmds/player/consider.c
@@ -3,14 +3,14 @@ inherit M_COMMAND;
 string *usage(void) {
    string *lines;
 
-   lines = ({ "Usage: attack [-h] [OBJECT]" });
+   lines = ({ "Usage: con [-h] [OBJECT]" });
    lines += ({ "" });
-   lines += ({ "OBJ offends you, you are going to destroy it." });
+   lines += ({ "OBJ offends you, you are deciding if you can kill it." });
    lines += ({ "" });
    lines += ({ "Options:" });
    lines += ({ "\t-h\tHelp, this usage message." });
    lines += ({ "Examples:" });
-   lines += ({ "\tattack rat" });
+   lines += ({ "\tconsider rat" });
 
    lines += get_alsos();
 
@@ -26,13 +26,12 @@ void setup_alsos() {
    add_also("player", "pray");
    add_also("player", "query");
    add_also("player", "quit");
-   add_also("player", "rest");
-   add_also("player", "stand");
    add_also("player", "wimpy");
 }
 
 static void main(string str) {
    object obj;
+   int level, leveldelta, yourlevel;
 
    if (!alsos) {
       setup_alsos();
@@ -53,24 +52,19 @@ static void main(string str) {
       return;
    }
 
-   if (this_player()->is_resting()) {
-      write("You can not do that when your are resting.\n");
-      return;
-   }
-
    obj = this_player()->query_environment()->present(lowercase(str));
    if (!obj) {
-      write("Attack what?\n");
+      write("Consider what?\n");
       return;
    }
 
    if (!obj->is_living() || obj->is_dead()) {
-      write("You can not attack that.\n");
+      write("You can not consider attacking that.\n");
       return;
    }
 
    if (obj == this_player()) {
-      write("You can not attack yourself.\n");
+      write("You can not consider attacking yourself.\n");
       return;
    }
 
@@ -87,10 +81,29 @@ static void main(string str) {
    }
 
    if(obj->query_environment()->query_nokill() == 1) {
-     write("You can't kill anything in here, this is holy ground!");
+     write("You can't kill anything in here, this is unholy ground!");
      return;
    }
 
-   this_player()->targeted_action("$N $vattack $t.", obj);
-   this_player()->attack(obj);
+   level = obj->query_level();
+   /*write("Level is: " + level + "\n");*/
+
+   yourlevel = this_player()->query_level();
+   leveldelta = (level-yourlevel);
+   /*write("Level difference is " + leveldelta + "\n");*/
+
+  if(leveldelta < 0){
+    write("It will be an easy kill.\n");
+  }else if(leveldelta == 0){
+    write("Perfect match!\n");
+  }else if(leveldelta > 0 && leveldelta <=3){
+    write("It is a little stronger than you.\n");
+  }else if(leveldelta > 3 && leveldelta <=5){
+    write("It is a fair deal stronger than you.\n");
+  }else if (leveldelta > 5){
+    write("It is MUCHO stronger than you. How much, you don't know. You are fairly certain it will kill you though.\n");
+  }
+
+   return;
+
 }
